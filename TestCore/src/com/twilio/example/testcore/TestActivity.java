1cd8995105fff0a8d1f158f9c64207b8258da854
Kumkum Bagchi <kbagchi@twilio.com>
2015-03-24 23:37:18 -0700

    Addded Api to initialize library
    Former-commit-id: 39142101e6d0ac8a3aaec913933d87b8caccf0e2

--- TestCore/src/com/twilio/example/testcore/TestActivity.java
+++ TestCore/src/com/twilio/example/testcore/TestActivity.java
@@ -8,22 +8,29 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class TestActivity extends Activity {
 	
 	SignalPhone phone;
+	private EditText clientNameTextBox;
+	private Button register;
+	private static final String DEFAULT_CLIENT_NAME = "jenny";
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_test);
-		Button button = (Button)findViewById(R.id.load);
-		phone = SignalPhone.getInstance(getApplicationContext());
-		button.setOnClickListener(new View.OnClickListener() {		
+		this.clientNameTextBox = (EditText)findViewById(R.id.client_name);
+		this.clientNameTextBox.setText(DEFAULT_CLIENT_NAME);
+		this.register = (Button)findViewById(R.id.register);
+		this.phone = SignalPhone.getInstance(getApplicationContext());
+		this.register.setOnClickListener(new View.OnClickListener() {		
 			@Override
 			public void onClick(View v) {		
-				TestActivity.this.phone.login("Kumkum");
+				TestActivity.this.phone.login(clientNameTextBox.getText().toString());
 			}
 		});
 	}
