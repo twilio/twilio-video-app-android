bbb319c3c9e82573a04bd8f924d2d18aa7ce7625
Nikola Novakovic <nnovakovic@twilio.com>
2015-08-04 14:48:24 -0700

    PREFIX variable points to sdk build tools installation folder
    Former-commit-id: 21dc6a090fc457628348af71076c0e6fb395b126

--- sdk/jni/signal-core.mk
+++ sdk/jni/signal-core.mk
@@ -3,11 +3,16 @@ ifneq ($(TARGET_SIMULATOR),true)
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+
+ifeq ($(PREFIX),)
+PREFIX:= /usr/local/twilio-sdk
+endif
+
 LOCAL_MODULE            := twilio-sdk-core
-LOCAL_SRC_FILES         := /usr/local/twilio-sdk/core/android/armeabiv7a/lib/libtwilio-sdk-core-full.a
-LOCAL_EXPORT_C_INCLUDES := /usr/local/twilio-sdk/core/android/armeabiv7a/include \
-                           /usr/local/twilio-sdk/webrtc/android/armeabiv7a/include \
-                           /usr/local/twilio-sdk/pjsip/android/armeabiv7a/include
+LOCAL_SRC_FILES         := $(PREFIX)/core/android/armeabiv7a/lib/libtwilio-sdk-core-full.a
+LOCAL_EXPORT_C_INCLUDES := $(PREFIX)/core/android/armeabiv7a/include \
+                           $(PREFIX)/webrtc/android/armeabiv7a/include \
+                           $(PREFIX)/pjsip/android/armeabiv7a/include
 LOCAL_EXPORT_CFLAGS := -Wall -DARM -DPJ_IS_LITTLE_ENDIAN=1 -DPJ_IS_BIG_ENDIAN=0 -DWEBRTC_POSIX -DWEBRTC_ANDROID -DWEBRTC_ANDROID_OPENSLES -DPOCO_ANDROID
 include $(PREBUILT_STATIC_LIBRARY)
 
