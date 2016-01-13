58759c74f4ec26be687f71006fe65ee13588594b
Nikola Novakovic <nnovakovic@twilio.com>
2016-01-13 15:35:44 -0800

    Removing pinning from promote-rc-to-release script and putting into new script called pin-release.sh. It will be separate task in Jenkins
    Fixed local package paths.
    
    
    Former-commit-id: 761223f95ad5963c7390eaa23476565a4596f4f9

--- scripts/promote-rc-to-release.sh
+++ scripts/promote-rc-to-release.sh
@@ -64,36 +64,9 @@ fi
 rm -rf ${SCRIPTS_DIR}/downloads
 mkdir ${SCRIPTS_DIR}/downloads
 
-MAJORMINOR=$(echo $SDK_RELEASE_VERSION| cut -d'.' -f-2)
 DIRECT_URL="https://media.twiliocdn.com/sdk/android/conversations/releases/${SDK_RELEASE_VERSION}/twilio-conversations-android-${SDK_RELEASE_VERSION}.tar.bz2"
-MAJORMINOR_URL="https://media.twiliocdn.com/sdk/android/conversations/v${MAJORMINOR}/twilio-conversations-android.tar.bz2"
-LATEST_URL="https://media.twiliocdn.com/sdk/android/conversations/latest/twilio-conversations-android.tar.bz2"
 
-curl $DIRECT_URL --output ${SCRIPTS_DIR}/downloads/twilio-rtc-conversations-direct.tar.bz2
-md5 ${SDK_PACKAGE_PATH}/sdk/rtc/android/conversations/releases/${SDK_RELEASE_VERSION}/twilio-rtc-conversations.tar.bz2 ${SCRIPTS_DIR}/downloads/twilio-rtc-conversations-direct.tar.bz2
+curl $DIRECT_URL --output ${SCRIPTS_DIR}/downloads/twilio-conversations-direct.tar.bz2
+md5 ${SDK_PACKAGE_PATH}/dist/${SDK_RELEASE_VERSION}/twilio-conversations-android.tar.bz2 ${SCRIPTS_DIR}/downloads/twilio-conversations-direct.tar.bz2
 
-echo "sdk-release-tool: updating..."
-./sdk-release-tool pin --prod twilio-conversations-android ${SDK_RELEASE_VERSION}
-if [ "$?" -ne "0" ]; then
-    echo "Error: failed to execute sdk-release-tool update"
-    exit 1
-fi
-
-echo "sdk-release-tool: pin latest..."
-./sdk-release-tool pin-latest --prod twilio-conversations-android ${SDK_RELEASE_VERSION}
-if [ "$?" -ne "0" ]; then
-    echo "Error: failed to execute sdk-release-tool pin latest"
-    exit 1
-fi
-
-echo "Testing whether versions are same..."
-curl -L $MAJORMINOR_URL --output ${SCRIPTS_DIR}/downloads/twilio-rtc-conversations-majorminor.tar.bz2
-curl -L $LATEST_URL --output ${SCRIPTS_DIR}/downloads/twilio-rtc-conversations-latest.tar.bz2
-
-md5 ${SDK_PACKAGE_PATH}/sdk/rtc/android/conversations/releases/${SDK_RELEASE_VERSION}/twilio-rtc-conversations.tar.bz2 ${SCRIPTS_DIR}/downloads/twilio-rtc-conversations-direct.tar.bz2 ${SCRIPTS_DIR}/downloads/twilio-rtc-conversations-majorminor.tar.bz2 ${SCRIPTS_DIR}/downloads/twilio-rtc-conversations-latest.tar.bz2
 popd
-
-echo "Major minor version: $MAJORMINOR"
-echo "Direct URL: $DIRECT_URL"
-echo "Major Minor URL: $MAJORMINOR_URL"
-echo "Latest URL: $LATEST_URL"
