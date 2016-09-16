8f36a4601ef3cd10d8f270ebf9204fd492588c17
Aaron Alaniz <aalaniz@twilio.com>
2016-09-16 02:32:38 -0500

    javadocing video capturer
    Former-commit-id: f60f7ef31cb0a87b68794aa557dfe03182370150

--- video/src/main/java/com/twilio/video/VideoCapturer.java
+++ video/src/main/java/com/twilio/video/VideoCapturer.java
@@ -2,14 +2,40 @@ package com.twilio.video;
 
 import java.util.List;
 
+/**
+ * Generic video capturing interface.
+ */
 public interface VideoCapturer {
+    /**
+     * Returns list of all supported video formats this capturer supports.
+     */
     List<VideoFormat> getSupportedFormats();
+
+    /**
+     * Start capturing frames.
+     *
+     * @param captureFormat format to start capturing in
+     * @param capturerListener consumes frames upon availability
+     */
     void startCapture(VideoFormat captureFormat,
                       Listener capturerListener);
+
+    /**
+     * Stop capturing. This method must block until capturer has stopped.
+     */
     void stopCapture();
 
     interface Listener {
+        /**
+         * Notify whether capturer started
+         *
+         * @param success true if capturer successfully started or false if there was a failure
+         */
         void onCapturerStarted(boolean success);
+
+        /**
+         * Provides available video frame
+         */
         void onFrameCaptured(VideoFrame videoFrame);
     }
 }
