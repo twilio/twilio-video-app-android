cf93e511bbf39ac79c8db4245051d487f3001941
Aaron Alaniz <aalaniz@twilio.com>
2016-09-16 02:04:43 -0500

    Javadocing local participant and simplifying a bit more
    Former-commit-id: 094a56ac01a6d8e20a947a587b5ec00166569cb7

--- video/src/main/java/com/twilio/video/LocalParticipant.java
+++ video/src/main/java/com/twilio/video/LocalParticipant.java
@@ -1,26 +1,16 @@
 package com.twilio.video;
 
+/**
+ * Represents the local participant of a {@link Room} and is provided upon connected.
+ */
 public class LocalParticipant {
-
-    final private String sid;
-    final private String identity;
-    final private LocalMedia localMedia;
+    public final String sid;
+    public final String identity;
+    public final LocalMedia localMedia;
 
     LocalParticipant(String sid, String identity, LocalMedia localMedia) {
         this.sid = sid;
         this.identity = identity;
         this.localMedia = localMedia;
-    }
-
-    public String getSid() {
-        return sid;
-    }
-
-    public String getIdentity() {
-        return identity;
-    }
-
-    public LocalMedia getLocalMedia() {
-        return localMedia;
     }
 }
