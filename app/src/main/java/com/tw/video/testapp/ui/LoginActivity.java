7aaf7b5c93d0e4c8b9506cf86f661e29cd87814a
Aleksandr Dolmatov <sashjakk@Aleksandrs-MacBook-Pro.local>
2016-10-03 11:26:26 +0300

    Add splash screen
    Former-commit-id: 1df569493bdb6152d0cc153fb14dc1e2476090cd

--- app/src/main/java/com/tw/video/testapp/ui/LoginActivity.java
+++ app/src/main/java/com/tw/video/testapp/ui/LoginActivity.java
@@ -44,6 +44,7 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
