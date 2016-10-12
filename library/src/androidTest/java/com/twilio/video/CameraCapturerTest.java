d99bb64a8f9d770582eb8c715bac3d03d1ecb963
Aaron Alaniz <aalaniz@twilio.com>
2016-10-12 12:14:50 -0500

    Removing extraneous test suites
    Former-commit-id: d3a1881fcafe2bac0bf9666bfc518e0858938a74

--- library/src/androidTest/java/com/twilio/video/CameraCapturerTest.java
+++
@@ -1,9 +0,0 @@
-package com.twilio.video;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({CameraCapturerBaseTest.class, CameraCapturerParameterizedTest.class})
-public class CameraCapturerTest {
-}
