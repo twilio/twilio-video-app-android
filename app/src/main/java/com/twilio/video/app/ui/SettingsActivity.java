68e903ec4b9a6d3a2494895c91a694e813a9d2ab
Aleksandr Dolmatov <sashjakk@Aleksandrs-MacBook-Pro.local>
2016-11-24 00:10:48 +0200

    Display SettingsActivity label and set home activity
    Former-commit-id: 2e70532fa9bb6dca3023854b4f92db208525cf75

--- app/src/main/java/com/twilio/video/app/ui/SettingsActivity.java
+++ app/src/main/java/com/twilio/video/app/ui/SettingsActivity.java
@@ -2,8 +2,10 @@ package com.twilio.video.app.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.MenuItem;
 
 import com.twilio.video.app.R;
 
@@ -13,10 +15,26 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
