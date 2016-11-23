3a2ae30a5ee59be8a086dffc175d45a87a2e0df5
Aleksandr Dolmatov <sashjakk@Aleksandrs-MacBook-Pro.local>
2016-11-23 23:41:02 +0200

    Add SettingsActivity with media constraints preferences
    Former-commit-id: 6c0d914c5e832ac28d32ed17ddb969ccc67e0d36

---
+++ app/src/main/java/com/twilio/video/app/ui/SettingsActivity.java
@@ -0,0 +1,29 @@
+package com.twilio.video.app.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+import com.twilio.video.app.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
