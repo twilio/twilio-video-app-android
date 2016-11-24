cd5d066fef86c215bb9759565d68a79546be7a2f
Aleksandr Dolmatov <sashjakk@Aleksandrs-MacBook-Pro.local>
2016-11-24 11:57:59 +0200

    Added separate SettingsActivity theme, removed cancel button from aspect ratio
    Former-commit-id: cd5efc7057259e0aed068f3e823e663557039902

--- app/src/main/java/com/twilio/video/app/ui/SettingsActivity.java
+++ app/src/main/java/com/twilio/video/app/ui/SettingsActivity.java
@@ -2,7 +2,6 @@ package com.twilio.video.app.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.view.MenuItem;
@@ -14,11 +13,6 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
 
         getSupportFragmentManager()
                 .beginTransaction()
