852ccf7267af87c74fe6c50dc22e42c62e88b990
Aaron Alaniz <aalaniz@twilio.com>
2016-12-07 11:20:05 -0600

    Add realm and topology preference
    Former-commit-id: e8dc3114f5f3c1ec54f3f31bcb10baeecd788193

--- app/src/main/java/com/twilio/video/app/data/Preferences.java
+++ app/src/main/java/com/twilio/video/app/data/Preferences.java
@@ -1,6 +1,8 @@
 package com.twilio.video.app.data;
 
 public class Preferences {
+    public static final String REALM = "pref_realm";
+    public static final String TOPOLOGY = "pref_topology";
     public static final String MIN_FPS = "pref_min_fps";
     public static final String MAX_FPS = "pref_max_fps";
     public static final String MIN_VIDEO_DIMENSIONS = "pref_min_video_dim";
