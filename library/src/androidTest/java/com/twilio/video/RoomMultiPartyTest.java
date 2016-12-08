7f1828207558eb32e93332e39dddbec1fc490b20
Nikola Novakovic <nnovakovic@twilio.com>
2016-12-08 14:33:12 -0800

    Added unit tests that exercise GSDK-984 scenario (#499)
    * Added unit tests that exercise GSDK-984 scenario.
    
    
    Former-commit-id: 19abfe0fbe32fed3fed81d0b718c6797c0e9aa37

---
+++ library/src/androidTest/java/com/twilio/video/RoomMultiPartyTest.java
@@ -0,0 +1,121 @@
+package com.twilio.video;
+
+import android.content.Context;
+import android.provider.MediaStore;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Pair;
+
+import com.twilio.video.base.BaseClientTest;
+import com.twilio.video.helper.CallbackHelper;
+import com.twilio.video.util.AccessTokenUtils;
+import com.twilio.video.util.RandUtils;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class RoomMultiPartyTest extends BaseClientTest {
+    private static final int PARTICIPANT_NUM = 3;
+
+    private Context context;
+    private List<VideoClient> videoClients;
+    private List<Pair<Room, CallbackHelper.FakeRoomListener>> rooms;
+    private String roomName;
+
+    @Before
+    public void setup() throws InterruptedException {
+        super.setup();
+        context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        rooms = new ArrayList<>();
+        videoClients = new ArrayList<>();
+        for (int i = 0; i < PARTICIPANT_NUM; i++) {
+            String token = AccessTokenUtils.getAccessToken(RandUtils.generateRandomString(10));
+            videoClients.add(new VideoClient(context, token));
+        }
+        roomName = RandUtils.generateRandomString(20);
+    }
+
+    @After
+    public void teardown() throws InterruptedException {
+        for (Pair<Room, CallbackHelper.FakeRoomListener> roomPair : rooms) {
+            roomPair.second.onDisconnectedLatch = new CountDownLatch(1);
+            roomPair.first.disconnect();
+            roomPair.second.onDisconnectedLatch.await(10, TimeUnit.SECONDS);
+        }
+        rooms.clear();
+    }
+
+    @Test
+    public void shouldHaveCorrectParticipantCount() throws InterruptedException {
+        for (VideoClient client : videoClients) {
+            CallbackHelper.FakeRoomListener roomListener = new CallbackHelper.FakeRoomListener();
+            roomListener.onConnectedLatch = new CountDownLatch(1);
+            int numberOfParticipants = rooms.size();
+
+            // add listener to all other participants
+            for (Pair<Room, CallbackHelper.FakeRoomListener> roomPair : rooms) {
+                roomPair.second.onParticipantConnectedLatch = new CountDownLatch(1);
+            }
+
+            Room room = createRoom(client, roomListener, roomName);
+            assertTrue(roomListener.onConnectedLatch.await(20, TimeUnit.SECONDS));
+            assertEquals(numberOfParticipants, room.getParticipants().size());
+
+            // check if all participants got notification
+            for (Pair<Room, CallbackHelper.FakeRoomListener> roomPair : rooms) {
+                assertTrue(roomPair.second.onParticipantConnectedLatch.await(10, TimeUnit.SECONDS));
+                assertEquals(numberOfParticipants, roomPair.first.getParticipants().size());
+            }
+
+            rooms.add(new Pair<Room, CallbackHelper.FakeRoomListener>(room, roomListener));
+        }
+    }
+
+    @Test
+    public void shouldNotHaveLocalParticipantInParticipantsList() throws InterruptedException {
+        for (VideoClient client : videoClients) {
+            CallbackHelper.FakeRoomListener roomListener = new CallbackHelper.FakeRoomListener();
+            roomListener.onConnectedLatch = new CountDownLatch(1);
+
+            Room room = createRoom(client, roomListener, roomName);
+            String localIdentity = room.getLocalParticipant().getIdentity();
+            String localSid = room.getLocalParticipant().getSid();
+
+            Map<String, Participant> participantMap = room.getParticipants();
+            assertFalse(participantMap.containsKey(localIdentity));
+            for (Participant participant : participantMap.values()) {
+                assertNotEquals(localSid, participant.getSid());
+            }
+            rooms.add(new Pair<Room, CallbackHelper.FakeRoomListener>(room, roomListener));
+        }
+    }
+
+    private Room createRoom(VideoClient videoClient, CallbackHelper.FakeRoomListener listener,
+                            String roomName) throws InterruptedException {
+        listener.onConnectedLatch = new CountDownLatch(1);
+
+        ConnectOptions connectOptions = new ConnectOptions.Builder()
+                .roomName(roomName)
+                .build();
+        Room room = videoClient.connect(connectOptions, listener);
+        assertTrue(listener.onConnectedLatch.await(20, TimeUnit.SECONDS));
+        return room;
+    }
+}
