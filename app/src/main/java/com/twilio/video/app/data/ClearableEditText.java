671f2ea080ecff35822d44af3800b75afc2c098c
Aleksandr Dolmatov <adolmatovs@twilio.com>
2016-12-08 17:09:25 +0200

    Add clearable edit text
    Former-commit-id: cc0fc119063f3f4ffacf298a1fd0d1bec05e895d

---
+++ app/src/main/java/com/twilio/video/app/data/ClearableEditText.java
@@ -0,0 +1,125 @@
+package com.twilio.video.app.data;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.twilio.video.app.R;
+
+/**
+ * ClearableEditText is an extension for standard EditText with an extra option to setup clear icon
+ * with as right compound drawable, which handles clear icon touch event as erase for the contents
+ * of user input.
+ *
+ * @attr name clearIcon - clear action icon to display to the right of EditText input.
+ */
+public class ClearableEditText extends AppCompatEditText {
+
+    /**
+     * Clear icon resource id.
+     */
+    private int clearIconResId;
+
+    /**
+     * Clear icon drawable.
+     */
+    private Drawable clearDrawable;
+
+    public ClearableEditText(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public ClearableEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public ClearableEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+
+        if (attrs != null) {
+            TypedArray stylables = context.getTheme()
+                    .obtainStyledAttributes(attrs, R.styleable.ClearableEditText, 0, 0);
+
+            // obtain clear icon resource id
+            clearIconResId = stylables.getResourceId(R.styleable.ClearableEditText_clearIcon, -1);
+            if (clearIconResId != -1) {
+                clearDrawable = ContextCompat.getDrawable(context, clearIconResId);
+            }
+        }
+
+        // setup initial clear icon state
+        setCompoundDrawablesWithIntrinsicBounds(null, null, clearDrawable, null);
+        showClearIcon(getText().toString().length() > 0);
+
+        // update clear icon state after every text change
+        addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                showClearIcon(editable.toString().length() > 0);
+            }
+        });
+
+        // simulate on clear icon click - delete edit text contents
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                if (isClearVisible() && motionEvent.getAction() == MotionEvent.ACTION_UP) {
+
+                    ClearableEditText editText = (ClearableEditText) view;
+                    Rect bounds = clearDrawable.getBounds();
+
+                    if (motionEvent.getRawX() >= (view.getRight() - bounds.width())) {
+                        editText.setText("");
+                    }
+                }
+
+                return false;
+            }
+        });
+    }
+
+    /**
+     * Displays clear icon in ClearableEditText.
+     *
+     * @param show pass true to display icon, otherwise false to hide.
+     */
+    public void showClearIcon(boolean show) {
+        // TODO: should probably use setVisibility method, but seems to not working.
+        if (clearDrawable != null) {
+            clearDrawable.setAlpha(show ? 255 : 0);
+        }
+    }
+
+    /**
+     * Reflects current state of clear icon.
+     *
+     * @return true if active, otherwise - false.
+     */
+    public boolean isClearVisible() {
+        return clearDrawable != null && DrawableCompat.getAlpha(clearDrawable) == 255;
+    }
+}
