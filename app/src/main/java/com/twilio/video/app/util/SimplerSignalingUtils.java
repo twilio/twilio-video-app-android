478f9875011d10f76d79829949c9d2ccb3607308
Aaron Alaniz <aalaniz@twilio.com>
2016-12-08 00:45:26 -0600

    Use constants in simpler signaling utils
    Former-commit-id: 50c7635b7ab273ab04fc6d6068fd04cbe370346f

--- app/src/main/java/com/twilio/video/app/util/SimplerSignalingUtils.java
+++ app/src/main/java/com/twilio/video/app/util/SimplerSignalingUtils.java
@@ -5,7 +5,6 @@ import android.util.Base64;
 import com.google.gson.GsonBuilder;
 import com.twilio.video.app.model.TwilioIceResponse;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,15 +17,20 @@ import retrofit.http.QueryMap;
 
 public class SimplerSignalingUtils {
     private static final String P2P = "P2P";
-    public static final String PROD = "prod";
-    public static final String STAGE = "stage";
-    public static final String DEV= "dev";
-
+    private static final String PROD = "prod";
+    private static final String ENVIRONMENT = "environment";
+    private static final String IDENTITY = "identity";
+    private static final String TTL = "ttl";
     /*
      * The default is usually 30 minutes. We are intentionally setting it to 5 minutes to validate
      * expiration.
      */
-    public static final String TTL = "300";
+    private static final String TTL_DEFAULT = "300";
+    private static final String CONFIGURATION_PROFILE_SID = "configurationProfileSid";
+    public static final String STAGE = "stage";
+    public static final String DEV= "dev";
+
+
 
     /* Define the Retrofit Token Service */
     interface SimplerSignalingApi {
@@ -66,10 +70,10 @@ public class SimplerSignalingUtils {
                                       String topology,
                                       Callback<String> callback) {
         HashMap<String,String> options = new HashMap<>();
-        options.put("environment", environment);
-        options.put("identity", username);
-        options.put("ttl", TTL);
-        options.put("configurationProfileSid", getProfileConfigSid(environment, topology));
+        options.put(ENVIRONMENT, environment);
+        options.put(IDENTITY, username);
+        options.put(TTL, TTL_DEFAULT);
+        options.put(CONFIGURATION_PROFILE_SID, getProfileConfigSid(environment, topology));
         simplerSignalingService.getAccessToken(options, callback);
     }
 
@@ -77,12 +81,18 @@ public class SimplerSignalingUtils {
         boolean isP2P = topology.equals(P2P);
         switch(environment) {
             case DEV:
-                return isP2P ? "VSbf4c8aee1e259d11b2c5adeebb7c0dbe" : "VS6469e95f0b2e2c8f931086988d69f815";
+                return isP2P ?
+                        "VSbf4c8aee1e259d11b2c5adeebb7c0dbe" :
+                        "VS6469e95f0b2e2c8f931086988d69f815";
             case STAGE:
-                return isP2P ? "VS0d1c1b07fafbe94b73670b37e7aedfbb" : "VS395e1a612a6e3c63100a3b4d99d52265";
+                return isP2P ?
+                        "VS0d1c1b07fafbe94b73670b37e7aedfbb" :
+                        "VS395e1a612a6e3c63100a3b4d99d52265";
             case PROD:
             default:
-                return isP2P ? "VS3f75e0f14e7c8b20938fc5092e82f23a" : "VS25275758820071c0d42246c538bc11ad";
+                return isP2P ?
+                        "VS3f75e0f14e7c8b20938fc5092e82f23a" :
+                        "VS25275758820071c0d42246c538bc11ad";
         }
     }
 
