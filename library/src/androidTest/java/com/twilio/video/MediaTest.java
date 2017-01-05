f3a3d1f2a7cdf6dc8952800d63bbfcf5d8cc2123
Aaron Alaniz <aalaniz@twilio.com>
2017-01-05 13:10:57 -0600

    Reduces MediaTest by consolidating track events into one test
    Former-commit-id: 1daa3cd5490f83334e68dc847d41cb0d88ebff0b

--- library/src/androidTest/java/com/twilio/video/MediaTest.java
+++ library/src/androidTest/java/com/twilio/video/MediaTest.java
@@ -18,103 +18,48 @@ import static org.junit.Assert.assertTrue;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MediaTest extends BaseMediaTest {
-
     @Test
-    public void shouldReceiveOnAudioTrackAdded() throws InterruptedException {
+    public void shouldReceiveTrackEvents() throws InterruptedException {
+        // Audio track added
         CallbackHelper.FakeMediaListener mediaListener = new CallbackHelper.FakeMediaListener();
         mediaListener.onAudioTrackAddedLatch = new CountDownLatch(1);
         participant.getMedia().setListener(mediaListener);
-
         LocalAudioTrack audioTrack = actor2LocalMedia.addAudioTrack(true);
         assertTrue(mediaListener.onAudioTrackAddedLatch.await(20, TimeUnit.SECONDS));
-    }
 
-    @Test
-    public void shouldReceiveOnAudioTrackRemoved() throws InterruptedException {
-        CallbackHelper.FakeMediaListener mediaListener = new CallbackHelper.FakeMediaListener();
-        mediaListener.onAudioTrackAddedLatch = new CountDownLatch(1);
-        mediaListener.onAudioTrackRemovedLatch = new CountDownLatch(1);
-        participant.getMedia().setListener(mediaListener);
-
-        LocalAudioTrack audioTrack = actor2LocalMedia.addAudioTrack(true);
-        assertTrue(mediaListener.onAudioTrackAddedLatch.await(20, TimeUnit.SECONDS));
-        actor2LocalMedia.removeAudioTrack(audioTrack);
-        assertTrue(mediaListener.onAudioTrackRemovedLatch.await(20, TimeUnit.SECONDS));
-    }
-
-    @Test
-    public void shouldReceiveOnAudioTrackEnabled() throws InterruptedException {
-        CallbackHelper.FakeMediaListener mediaListener = new CallbackHelper.FakeMediaListener();
-        mediaListener.onAudioTrackAddedLatch = new CountDownLatch(1);
-        mediaListener.onAudioTrackEnabledLatch = new CountDownLatch(1);
-        participant.getMedia().setListener(mediaListener);
-
-        LocalAudioTrack audioTrack = actor2LocalMedia.addAudioTrack(false);
-        assertTrue(mediaListener.onAudioTrackAddedLatch.await(20, TimeUnit.SECONDS));
-        audioTrack.enable(true);
-        assertTrue(mediaListener.onAudioTrackEnabledLatch.await(20, TimeUnit.SECONDS));
-    }
-
-    @Test
-    public void shouldReceiveOnAudioTrackDisabled() throws InterruptedException {
-        CallbackHelper.FakeMediaListener mediaListener = new CallbackHelper.FakeMediaListener();
-        mediaListener.onAudioTrackAddedLatch = new CountDownLatch(1);
+        // Audio track disabled
         mediaListener.onAudioTrackDisabledLatch = new CountDownLatch(1);
-        participant.getMedia().setListener(mediaListener);
-
-        LocalAudioTrack audioTrack = actor2LocalMedia.addAudioTrack(true);
-        assertTrue(mediaListener.onAudioTrackAddedLatch.await(20, TimeUnit.SECONDS));
         audioTrack.enable(false);
         assertTrue(mediaListener.onAudioTrackDisabledLatch.await(20, TimeUnit.SECONDS));
-    }
 
-    @Test
-    public void shouldReceiveOnVideoTrackAdded() throws InterruptedException {
-        CallbackHelper.FakeMediaListener mediaListener = new CallbackHelper.FakeMediaListener();
+        // Audio track enabled
+        mediaListener.onAudioTrackEnabledLatch = new CountDownLatch(1);
+        audioTrack.enable(true);
+        assertTrue(mediaListener.onAudioTrackEnabledLatch.await(20, TimeUnit.SECONDS));
+
+        // Audio track removed
+        mediaListener.onAudioTrackRemovedLatch = new CountDownLatch(1);
+        actor2LocalMedia.removeAudioTrack(audioTrack);
+        assertTrue(mediaListener.onAudioTrackRemovedLatch.await(20, TimeUnit.SECONDS));
+
+        // Video track added
         mediaListener.onVideoTrackAddedLatch = new CountDownLatch(1);
-        participant.getMedia().setListener(mediaListener);
-
-        actor2LocalMedia.addVideoTrack(true, fakeVideoCapturer);
-        assertTrue(mediaListener.onVideoTrackAddedLatch.await(20, TimeUnit.SECONDS));
-    }
-
-    @Test
-    public void shouldReceiveOnVideoTrackRemoved() throws InterruptedException {
-        CallbackHelper.FakeMediaListener mediaListener = new CallbackHelper.FakeMediaListener();
-        mediaListener.onVideoTrackAddedLatch = new CountDownLatch(1);
-        mediaListener.onVideoTrackRemovedLatch = new CountDownLatch(1);
-        participant.getMedia().setListener(mediaListener);
-
         LocalVideoTrack videoTrack = actor2LocalMedia.addVideoTrack(true, fakeVideoCapturer);
         assertTrue(mediaListener.onVideoTrackAddedLatch.await(20, TimeUnit.SECONDS));
-        actor2LocalMedia.removeVideoTrack(videoTrack);
-        assertTrue(mediaListener.onVideoTrackRemovedLatch.await(20, TimeUnit.SECONDS));
-        assertTrue(true);
-    }
 
-    @Test
-    public void shouldReceiveOnVideoTrackEnabled() throws InterruptedException {
-        CallbackHelper.FakeMediaListener mediaListener = new CallbackHelper.FakeMediaListener();
-        mediaListener.onVideoTrackAddedLatch = new CountDownLatch(1);
-        mediaListener.onVideoTrackEnabledLatch = new CountDownLatch(1);
-        participant.getMedia().setListener(mediaListener);
-
-        LocalVideoTrack videoTrack = actor2LocalMedia.addVideoTrack(false, fakeVideoCapturer);
-        assertTrue(mediaListener.onVideoTrackAddedLatch.await(20, TimeUnit.SECONDS));
-        videoTrack.enable(true);
-        assertTrue(mediaListener.onVideoTrackEnabledLatch.await(20, TimeUnit.SECONDS));
-    }
-
-    @Test
-    public void shouldReceiveOnVideoTrackDisabled() throws InterruptedException {
-        CallbackHelper.FakeMediaListener mediaListener = new CallbackHelper.FakeMediaListener();
-        mediaListener.onVideoTrackAddedLatch = new CountDownLatch(1);
+        // Video track disabled
         mediaListener.onVideoTrackDisabledLatch = new CountDownLatch(1);
-        participant.getMedia().setListener(mediaListener);
-
-        LocalVideoTrack videoTrack = actor2LocalMedia.addVideoTrack(true, fakeVideoCapturer);
-        assertTrue(mediaListener.onVideoTrackAddedLatch.await(20, TimeUnit.SECONDS));
         videoTrack.enable(false);
         assertTrue(mediaListener.onVideoTrackDisabledLatch.await(20, TimeUnit.SECONDS));
+
+        // Video track enabled
+        mediaListener.onVideoTrackEnabledLatch = new CountDownLatch(1);
+        videoTrack.enable(true);
+        assertTrue(mediaListener.onVideoTrackEnabledLatch.await(20, TimeUnit.SECONDS));
+
+        // Video track removed
+        mediaListener.onVideoTrackRemovedLatch = new CountDownLatch(1);
+        actor2LocalMedia.removeVideoTrack(videoTrack);
+        assertTrue(mediaListener.onVideoTrackRemovedLatch.await(20, TimeUnit.SECONDS));
     }
 }
