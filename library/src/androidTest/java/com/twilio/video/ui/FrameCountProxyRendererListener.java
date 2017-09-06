122c33e71861b5bc03503e8156b97a8983011272
Aaron Alaniz <aalaniz@twilio.com>
2017-09-06 11:47:58 -0500

    Failed to create egl context (#769)
    * Add test that validates a VideoView can be renderered in RecyclerView
    
    * Adding test to render video view to list view

---
+++ library/src/androidTest/java/com/twilio/video/ui/FrameCountProxyRendererListener.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2017 Twilio, inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.twilio.video.ui;
+
+import com.twilio.video.I420Frame;
+import com.twilio.video.VideoRenderer;
+import com.twilio.video.VideoView;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+/*
+ * Renderer that counts frames before forwarding them to a VideoView
+ */
+public class FrameCountProxyRendererListener implements VideoRenderer {
+    private final AtomicReference<CountDownLatch> frameArrived =
+            new AtomicReference<>(new CountDownLatch(1));
+    final VideoView videoView;
+
+    public FrameCountProxyRendererListener(VideoView videoView) {
+        this.videoView = videoView;
+    }
+
+    @Override
+    public void renderFrame(I420Frame frame) {
+        frameArrived.get().countDown();
+        videoView.renderFrame(frame);
+    }
+
+    public boolean waitForFrame(int timeoutMs) throws InterruptedException {
+        frameArrived.set(new CountDownLatch(1));
+        return frameArrived.get().await(timeoutMs, TimeUnit.MILLISECONDS);
+    }
+}
