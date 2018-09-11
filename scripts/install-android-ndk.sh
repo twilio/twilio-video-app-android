c85bca3174757bef03fb455349e1e55a0b7d78a2
Aaron Alaniz <aalaniz@twilio.com>
2018-09-10 21:33:13 -0500

    Update NDK in circle

--- scripts/install-android-ndk.sh
+++ scripts/install-android-ndk.sh
@@ -5,12 +5,12 @@ ANDROID_NDK_INSTALLED="/opt/android-ndk/installed"
 
 if [ ! -e $ANDROID_NDK_INSTALLED ]; then
   cd $ANDROID_NDK_HOME
-  wget -qq https://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip
-  unzip -o -qq android-ndk-r12b-linux-x86_64.zip
-  mv android-ndk-r12b/* .
+  wget -qq https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip
+  unzip -o -qq android-ndk-r16b-linux-x86_64.zip
+  mv android-ndk-r16b/* .
   touch $ANDROID_NDK_INSTALLED
-  rm -rf android-ndk-r12b
-  rm android-ndk-r12b-linux-x86_64.zip
+  rm -rf android-ndk-r16b
+  rm android-ndk-r16b-linux-x86_64.zip
 fi
 
 echo "export ANDROID_NDK_HOME=$ANDROID_NDK_HOME" >> $BASH_ENV
