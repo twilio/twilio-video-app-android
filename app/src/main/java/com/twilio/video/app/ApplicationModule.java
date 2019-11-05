65202da70c87ce23dba0c726953c99a63035ded2
John Qualls <jaltonqualls@twilio.com>
2019-11-05 16:26:39 -0700

    Replace 2017 license text with 2019

--- app/src/main/java/com/twilio/video/app/ApplicationModule.java
+++ app/src/main/java/com/twilio/video/app/ApplicationModule.java
@@ -1,7 +1,7 @@
 package com.twilio.video.app;
 
 /*
- * Copyright (C) 2017 Twilio, Inc.
+ * Copyright (C) 2019 Twilio, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
