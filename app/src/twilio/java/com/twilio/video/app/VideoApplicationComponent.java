44790343c544fa628007ddd35bdcac93f6656030
John Qualls <jaltonqualls@twilio.com>
2019-12-06 17:05:29 -0700

    Fix compoment compilation errors in release variant

--- app/src/twilio/java/com/twilio/video/app/VideoApplicationComponent.java
+++ app/src/twilio/java/com/twilio/video/app/VideoApplicationComponent.java
@@ -3,6 +3,7 @@ package com.twilio.video.app;
 import com.twilio.video.app.auth.AuthModule;
 import com.twilio.video.app.data.DataModule;
 import com.twilio.video.app.data.api.VideoAppServiceModule;
+import com.twilio.video.app.idlingresource.IdlingResourceModule;
 import com.twilio.video.app.ui.ScreenSelectorModule;
 import com.twilio.video.app.ui.login.LoginActivityModule;
 import com.twilio.video.app.ui.room.RoomActivityModule;
@@ -22,7 +23,10 @@ import dagger.Component;
         SplashActivityModule.class,
         LoginActivityModule.class,
         RoomActivityModule.class,
-        SettingsActivityModule.class
+        SettingsActivityModule.class,
+        IdlingResourceModule.class
     }
 )
-public interface VideoApplicationComponent extends VideoApplicationGraph {}
+public interface VideoApplicationComponent {
+    void inject(VideoApplication application);
+}
