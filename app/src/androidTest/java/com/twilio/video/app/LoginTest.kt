ff3876a4eca8312a1ba5e00a10557c144681aaf8
John Qualls <jaltonqualls@twilio.com>
2019-12-09 16:45:59 -0700

    Remove hard coded UI test credentials

--- app/src/androidTest/java/com/twilio/video/app/LoginTest.kt
+++ app/src/androidTest/java/com/twilio/video/app/LoginTest.kt
@@ -1,29 +1,24 @@
 package com.twilio.video.app
 
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
-import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItem
-import androidx.test.espresso.idling.CountingIdlingResource
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
-import com.squareup.rx2.idler.Rx2Idler
-import com.twilio.video.app.idlingresource.IdlingResourceModule
+import com.google.gson.Gson
+import com.google.gson.stream.JsonReader
 import com.twilio.video.app.ui.splash.SplashActivity
-import io.reactivex.plugins.RxJavaPlugins
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.InputStreamReader
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -31,18 +26,26 @@ class LoginTest {
 
     @get:Rule
     var scenario = activityScenarioRule<SplashActivity>()
+    private lateinit var emailCredentials: EmailCredentials
 
     @Test
     fun `it_should_login_successfully_with_email_and_then_logout`() {
+            retrieveEmailCredentials()
             loginWithEmail()
             logout()
             onView(withId(R.id.google_sign_in_button)).check(matches(isDisplayed()))
     }
 
+    private fun retrieveEmailCredentials() {
+        val reader = InputStreamReader(getInstrumentation().context.assets.open("Credentials/TestCredentials.json"))
+        val jsonReader = JsonReader(reader)
+        emailCredentials = (Gson().fromJson(jsonReader, TestCredentials::class.java) as TestCredentials).email_sign_in_user
+    }
+
     private fun loginWithEmail() {
         onView(withId(R.id.email_sign_in_button)).perform(click())
-        onView(withId(R.id.email_edittext)).perform(typeText("androidvideoappuitest@twilio.com"))
-        onView(withId(R.id.password_edittext)).perform(typeText("P@\$\$W0rd"))
+        onView(withId(R.id.email_edittext)).perform(typeText(emailCredentials.email))
+        onView(withId(R.id.password_edittext)).perform(typeText(emailCredentials.password))
         onView(withId(R.id.login_button)).perform(click())
     }
 
