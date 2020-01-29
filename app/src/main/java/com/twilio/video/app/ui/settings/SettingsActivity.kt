b16fcb4a9395afbf9ce56bda116e163715495e17
John Qualls <jaltonqualls@twilio.com>
2020-01-29 10:00:03 -0700

    Remove duplicate login activity launch

--- app/src/main/java/com/twilio/video/app/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/twilio/video/app/ui/settings/SettingsActivity.kt
@@ -17,7 +17,6 @@
 package com.twilio.video.app.ui.settings
 
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.MenuItem
@@ -45,7 +44,6 @@ import com.twilio.video.app.base.BaseActivity
 import com.twilio.video.app.data.NumberPreference
 import com.twilio.video.app.data.NumberPreferenceDialogFragmentCompat
 import com.twilio.video.app.data.Preferences
-import com.twilio.video.app.ui.ScreenSelector
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
@@ -77,8 +75,6 @@ class SettingsActivity : BaseActivity() {
 
         @Inject
         internal lateinit var sharedPreferences: SharedPreferences
-        @Inject
-        internal lateinit var screenSelector: ScreenSelector
         @Inject
         internal lateinit var authenticator: Authenticator
         private var identityPreference: EditTextPreference? = null
@@ -166,15 +162,12 @@ class SettingsActivity : BaseActivity() {
 
         private fun logout() {
             requireActivity().let { activity ->
-                val loginIntent = Intent(activity, screenSelector.loginScreen)
-
                 // Clear all preferences and set defaults
                 sharedPreferences.edit().clear().apply()
                 PreferenceManager.setDefaultValues(activity, R.xml.preferences, true)
 
                 // Return to login activity
                 authenticator.logout()
-                startActivity(loginIntent)
                 activity.finish()
             }
         }
