9e2bb172d3164366fc4e1531ba0942ca28f68dcc
John Qualls <jaltonqualls@twilio.com>
2020-02-14 16:39:32 -0700

    Remove unneeded modules from RoomManagerModule

--- app/src/main/java/com/twilio/video/app/ui/room/RoomManagerModule.kt
+++ app/src/main/java/com/twilio/video/app/ui/room/RoomManagerModule.kt
@@ -2,15 +2,11 @@ package com.twilio.video.app.ui.room
 
 import com.twilio.video.app.ApplicationModule
 import com.twilio.video.app.ApplicationScope
-import com.twilio.video.app.data.DataModule
-import com.twilio.video.app.data.api.VideoAppServiceModule
 import dagger.Module
 import dagger.Provides
 
 @Module(includes = [
-    ApplicationModule::class,
-    DataModule::class,
-    VideoAppServiceModule::class])
+    ApplicationModule::class])
 class RoomManagerModule {
 
     @Provides
