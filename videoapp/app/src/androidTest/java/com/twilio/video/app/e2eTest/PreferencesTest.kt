8e3ffcfd29a54d0ed1f6195c90eec68179e76218
Aaron Alaniz <aalaniz@twilio.com>
2020-02-28 15:11:14 -0600

    Squashed 'videoapp/' changes from 5553afb3..90a48df7 (#492)
    90a48df7 Bump version code [skip ci]
    ec2ddbf5 Update to Video Android 5.1.1 (#49)
    3f7d3b36 Bump version code [skip ci]
    a42f08cc Fix notification crash due to svg on api versions prior to 21 (#48)
    0b9d4e07 Bump version code [skip ci]
    d472a714 Move topology to advanced settings (#46)
    55877049 Bump version code [skip ci]
    fbd95ea4 Fix p2p default topology bug (#45)
    2508aa81 Bump version code [skip ci]
    0ed77aea Gradle plugin 3.6.0 update (#44)
    976567e7 Bump version code [skip ci]
    ccdbbdac AHOYAPPS-104: Background Room Management Support (#42)
    0cb91ed1 Bump version code [skip ci]
    2f73e5f9 Add issue and feature templates (#39)
    67c1618c Bump version code [skip ci]
    84611d9b Firebase static factory and auth providers rename (#38)
    a0194f32 Bump version code [skip ci]
    
    git-subtree-dir: videoapp
    git-subtree-split: 90a48df7bda355a1f5ac844c8263ece2aa230c49

---
+++ videoapp/app/src/androidTest/java/com/twilio/video/app/e2eTest/PreferencesTest.kt
@@ -0,0 +1,41 @@
+package com.twilio.video.app.e2eTest
+
+import android.preference.PreferenceManager
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.twilio.video.app.data.Preferences
+import com.twilio.video.app.data.api.model.Topology
+import com.twilio.video.app.screen.assertSettingsTitleIsVisible
+import com.twilio.video.app.screen.clickSettingsMenuItem
+import com.twilio.video.app.ui.splash.SplashActivity
+import com.twilio.video.app.util.getTargetContext
+import com.twilio.video.app.util.retryEspressoAction
+import com.twilio.video.app.util.uiDevice
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class PreferencesTest : BaseUITest() {
+
+    @get:Rule
+    var scenario = activityScenarioRule<SplashActivity>()
+
+    @Test
+    fun it_should_assert_correct_default_shared_preferences() {
+        retryEspressoAction { clickSettingsMenuItem() }
+
+        retryEspressoAction { assertSettingsTitleIsVisible() }
+
+        uiDevice().run {
+            pressBack()
+        }
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getTargetContext())
+        assertThat(sharedPreferences.getString(Preferences.TOPOLOGY, null), equalTo(Topology.GROUP.string))
+    }
+}
