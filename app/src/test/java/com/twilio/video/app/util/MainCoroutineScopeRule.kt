8cbcbeed2fe32d8fcb4e1a24e079b3471d9eb13b
John Qualls <jaltonqualls@twilio.com>
2020-03-06 15:47:57 -0700

    Add google library license

--- app/src/test/java/com/twilio/video/app/util/MainCoroutineScopeRule.kt
+++ app/src/test/java/com/twilio/video/app/util/MainCoroutineScopeRule.kt
@@ -1,6 +1,22 @@
 package com.twilio.video.app.util
 
 /*
+ * Copyright (C) 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * Source is from https://github.com/googlecodelabs/kotlin-coroutines/blob/master/coroutines-codelab/finished_code/src/test/java/com/example/android/kotlincoroutines/main/utils/MainCoroutineScopeRule.kt
  * Since this isn't available as part of a library yet.
  */
