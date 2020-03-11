20530786b25d8f5989b86856b47639cfbe76589f
John Qualls <jaltonqualls@twilio.com>
2020-03-11 17:48:05 -0600

    Token error regression fix (#53)
    * Fix token error message displaying when muting audio before joining a room
    
    * Add basic room joining tests with audio and video track muting

--- app/src/androidTest/java/com/twilio/video/app/screen/LobbyScreen.kt
+++ app/src/androidTest/java/com/twilio/video/app/screen/LobbyScreen.kt
@@ -5,15 +5,13 @@ import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.hasChildCount
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.twilio.video.app.R
 import com.twilio.video.app.util.getString
 import com.twilio.video.app.util.getTargetContext
-import org.hamcrest.CoreMatchers.allOf
 
 fun assertScreenIsDisplayed() {
     onView(withText(getString(R.string.join))).check(matches(isDisplayed()))
@@ -32,13 +30,20 @@ fun enterRoomName(roomName: String) {
     onView(withId(R.id.room_edit_text)).perform(typeText(roomName))
 }
 
+fun clickVideoButton() {
+    onView(withId(R.id.local_video_image_button)).perform(click())
+}
+
+fun clickMicButton() {
+    onView(withId(R.id.local_audio_image_button)).perform(click())
+}
+
 fun clickJoinRoomButton() {
     onView(withId(R.id.connect)).perform(click())
 }
 
 fun assertRoomIsConnected() {
-    onView(allOf(withId(R.id.participant_stub_image),
-            withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))).check(matches(isDisplayed()))
+    onView(withId(R.id.remote_video_thumbnails)).check(matches(hasChildCount(1)))
 }
 
 fun clickDisconnectButton() {
