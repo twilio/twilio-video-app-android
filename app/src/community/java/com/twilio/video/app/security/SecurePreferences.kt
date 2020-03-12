46f0256edfa65c42b2532cb9309331ec83e811b4
John Qualls <jaltonqualls@twilio.com>
2020-03-12 15:00:35 -0600

    Add SecurePreferences to allow encryption of preferences

---
+++ app/src/community/java/com/twilio/video/app/security/SecurePreferences.kt
@@ -0,0 +1,42 @@
+package com.twilio.video.app.util
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.facebook.android.crypto.keychain.AndroidConceal
+import com.facebook.android.crypto.keychain.SharedPrefsBackedKeyChain
+import com.facebook.crypto.Crypto
+import com.facebook.crypto.CryptoConfig
+import android.util.Base64
+import com.facebook.crypto.Entity
+
+class SecurePreferences(
+    context: Context,
+    private val preferences: SharedPreferences
+) {
+
+    private val entity: Entity = Entity.create(context.packageName)
+    private val crypto: Crypto
+
+    init {
+        val keyChain = SharedPrefsBackedKeyChain(context, CryptoConfig.KEY_256)
+        crypto = AndroidConceal.get().createCrypto256Bits(keyChain)
+    }
+
+    fun putString(key: String, value: String): Boolean {
+        return preferences.edit().putString(key, encrypt(value)).commit()
+    }
+
+    fun getString(key: String): String? {
+        val encryptedText: String? = preferences.getString(key, null)
+        return if (encryptedText != null) decrypt(encryptedText) else null
+    }
+
+    private fun encrypt(plainText: String): String {
+        val cipherText = crypto.encrypt(plainText.toByteArray(), entity)
+        return Base64.encodeToString(cipherText, Base64.DEFAULT)
+    }
+
+    private fun decrypt(encryptedText: String): String {
+        return String(crypto.decrypt(Base64.decode(encryptedText, Base64.DEFAULT), entity))
+    }
+}
