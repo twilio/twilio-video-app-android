0cea5284b27a27812076eae5aa14d58fa0e10b24
John Qualls <jaltonqualls@twilio.com>
2020-03-24 13:07:00 -0600

    Add unit tests for AuthServiceError

---
+++ app/src/testCommunity/java/com/twilio/video/app/data/api/AuthServiceErrorTest.kt
@@ -0,0 +1,32 @@
+package com.twilio.video.app.data.api
+
+import com.twilio.video.app.data.api.AuthServiceError.EXPIRED_PASSCODE_ERROR
+import com.twilio.video.app.data.api.AuthServiceError.INVALID_PASSCODE_ERROR
+import junitparams.JUnitParamsRunner
+import junitparams.Parameters
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(JUnitParamsRunner::class)
+class AuthServiceErrorTest {
+
+    fun params() =
+            arrayOf(
+                    arrayOf("passcode incorrect", INVALID_PASSCODE_ERROR),
+                    arrayOf("passcode expired", EXPIRED_PASSCODE_ERROR),
+                    arrayOf("bad input", null),
+                    arrayOf<Any?>(null, null)
+            )
+
+    @Parameters(method = "params")
+    @Test
+    fun `value should return the corresponding AuthServiceError enum`(
+        input: String?,
+        expectedValue: AuthServiceError?
+    ) {
+
+        assertThat(AuthServiceError.value(input), equalTo(expectedValue))
+    }
+}
