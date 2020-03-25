dd3485e277c820db99369b304c904c56477f1550
John Qualls <jaltonqualls@twilio.com>
2020-03-25 15:28:27 -0600

    Join room error handling (#66)
    * Update target SDK to 29
    
    * Update kotlin plugin to 1.3.71
    
    * Add expired passcode error message when passcode expires while attempting to join a room
    
    * Target SDK 28 for now on robolectric
    
    * Remove redundant margin right attribute
    
    * Revert "Update target SDK to 29"
    
    This reverts commit 6ee08c2ead8c0a05cdb1c96a75c9b2b2b465208a.
    
    * Add margin end attribute back in to room layout

--- app/src/main/java/com/twilio/video/app/ui/room/RoomEvent.kt
+++ app/src/main/java/com/twilio/video/app/ui/room/RoomEvent.kt
@@ -2,10 +2,11 @@ package com.twilio.video.app.ui.room
 
 import com.twilio.video.RemoteParticipant
 import com.twilio.video.Room
+import com.twilio.video.app.data.api.AuthServiceError
 
 sealed class RoomEvent(val room: Room? = null) {
-    object TokenError : RoomEvent()
     object Connecting : RoomEvent()
+    class TokenError(val serviceError: AuthServiceError? = null) : RoomEvent()
     class RoomState(room: Room) : RoomEvent(room)
     class ConnectFailure(room: Room) : RoomEvent(room)
     class ParticipantConnected(room: Room, val remoteParticipant: RemoteParticipant) : RoomEvent(room)
