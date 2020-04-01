10fb14e085a487e1123c79c877dd779fd7ca23b5
John Qualls <jaltonqualls@twilio.com>
2020-04-01 17:26:49 -0600

    Add Javadoc to AudioDeviceListener

--- audio-router/src/main/java/com/twilio/audio_router/AudioDeviceChangeListener.java
+++ audio-router/src/main/java/com/twilio/audio_router/AudioDeviceChangeListener.java
@@ -4,7 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import java.util.List;
 
+/** A listener that receives the most recently available {@link AudioDevice}s. */
 public interface AudioDeviceChangeListener {
+
+    /**
+     * Receives a list of the most recently available {@link AudioDevice}s. Also provides the
+     * currently selected {@link AudioDevice} from {@link AudioDeviceSelector}.
+     *
+     * @param audioDevices the list of {@link AudioDevice}s or {@code null} if none are available.
+     * @param selectedAudioDevice the currently selected device or {@code null} if none have been
+     *     selected.
+     */
     void onAvailableAudioDevices(
             @NonNull List<AudioDevice> audioDevices, @Nullable AudioDevice selectedAudioDevice);
 }
