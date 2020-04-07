91ac58020212684b0a883cbed8e2cc3d68ee54b5
John Qualls <jaltonqualls@twilio.com>
2020-04-07 16:53:41 -0600

    Add IllegalStateException empty test cases

--- audioswitch/src/test/java/com/twilio/audioswitch/AudioDeviceSelectorTest.kt
+++ audioswitch/src/test/java/com/twilio/audioswitch/AudioDeviceSelectorTest.kt
@@ -1,12 +1,57 @@
 package com.twilio.audioswitch
 
-import org.junit.Before
-
-import org.junit.Assert.*
+import android.content.Context
+import com.nhaarman.mockitokotlin2.mock
+import org.junit.Test
 
 class AudioDeviceSelectorTest {
 
-    @Before
-    fun setUp() {
+    val context = mock<Context>()
+    private val bluetoothController = BluetoothController(context)
+    private lateinit var audioDeviceSelector: AudioDeviceSelector
+
+    @Test
+    fun `stop should throw an IllegalStateException if current state is stopped`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `activate should throw an IllegalStateException if current state is stopped`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `deactivate should throw an IllegalStateException if current state is stopped`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `selectDevice should throw an IllegalStateException if current state is stopped`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `start should throw an IllegalStateException if current state is started`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `deactivate should throw an IllegalStateException if current state is started`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `start should throw an IllegalStateException if current state is activated`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `activate should throw an IllegalStateException if current state is activated`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `selectDevice should throw an IllegalStateException if current state is activated`() {
+        TODO("Not yet implemented")
     }
 }
