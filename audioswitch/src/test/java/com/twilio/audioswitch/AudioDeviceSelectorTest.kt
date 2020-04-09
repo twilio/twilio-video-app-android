8e6eed67277ea7e891b650deb55ae2d634c9e074
John Qualls <jaltonqualls@twilio.com>
2020-04-09 15:45:47 -0600

    Create unit tests for PreConnectedDeviceListener

--- audioswitch/src/test/java/com/twilio/audioswitch/AudioDeviceSelectorTest.kt
+++ audioswitch/src/test/java/com/twilio/audioswitch/AudioDeviceSelectorTest.kt
@@ -1,17 +1,11 @@
 package com.twilio.audioswitch
 
-import android.content.Context
-import com.nhaarman.mockitokotlin2.mock
 import org.junit.Test
 
 class AudioDeviceSelectorTest {
 
-    val context = mock<Context>()
-    private val bluetoothController = BluetoothController(context)
-    private lateinit var audioDeviceSelector: AudioDeviceSelector
-
     @Test
-    fun `stop should throw an IllegalStateException if current state is stopped`() {
+    fun `stop should throw an IllegalStateErxception if current state is stopped`() {
         TODO("Not yet implemented")
     }
 
