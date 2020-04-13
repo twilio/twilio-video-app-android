3fae05a7525996e219adf69ff7e245df17db134f
John Qualls <jaltonqualls@twilio.com>
2020-04-13 15:44:35 -0600

    Finish unit testing BluetoothDeviceReceiver

--- audioswitch/src/main/java/com/twilio/audioswitch/bluetooth/BluetoothDeviceReceiver.kt
+++ audioswitch/src/main/java/com/twilio/audioswitch/bluetooth/BluetoothDeviceReceiver.kt
@@ -1,11 +1,19 @@
 package com.twilio.audioswitch.bluetooth
 
-import android.bluetooth.BluetoothClass
+import android.bluetooth.BluetoothClass.Device.AUDIO_VIDEO_CAR_AUDIO
+import android.bluetooth.BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE
+import android.bluetooth.BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothDevice.ACTION_ACL_CONNECTED
+import android.bluetooth.BluetoothDevice.ACTION_ACL_DISCONNECTED
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.media.AudioManager
+import android.media.AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED
+import android.media.AudioManager.EXTRA_SCO_AUDIO_STATE
+import android.media.AudioManager.SCO_AUDIO_STATE_CONNECTED
+import android.media.AudioManager.SCO_AUDIO_STATE_DISCONNECTED
+import android.media.AudioManager.SCO_AUDIO_STATE_ERROR
 import com.twilio.audioswitch.LogWrapper
 
 private const val TAG = "BluetoothDeviceReceiver"
@@ -16,45 +24,58 @@ internal class BluetoothDeviceReceiver(
 ) : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        val action = intent.action
-        if (action != null) {
+        intent.action?.let { action ->
             when (action) {
-                BluetoothDevice.ACTION_ACL_CONNECTED -> {
-                    val connectedBluetoothDevice = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
-                    if (isHeadsetDevice(connectedBluetoothDevice)) {
+                ACTION_ACL_CONNECTED -> {
+                    intent.getHeadsetDevice()?.let { bluetoothDevice ->
                         logger.d(
                                 TAG,
-                                "Bluetooth " +
-                                        connectedBluetoothDevice.name +
+                                "Bluetooth ACL device " +
+                                        bluetoothDevice.name +
                                         " connected")
-                        deviceListener.onBluetoothConnected(connectedBluetoothDevice)
+                        deviceListener.onBluetoothConnected(bluetoothDevice)
                     }
                 }
-                BluetoothDevice.ACTION_ACL_DISCONNECTED -> {
-                    val disconnectedBluetoothDevice = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
-                    logger.d(TAG, "Bluetooth disconnected")
-                    deviceListener.onBluetoothDisconnected()
-                }
-                AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED -> {
-                    val state = intent.getIntExtra(
-                            AudioManager.EXTRA_SCO_AUDIO_STATE,
-                            AudioManager.SCO_AUDIO_STATE_ERROR)
-                    if (state == AudioManager.SCO_AUDIO_STATE_CONNECTED) {
-                        logger.d(TAG, "Bluetooth Sco Audio connected")
-                    } else if (state == AudioManager.SCO_AUDIO_STATE_DISCONNECTED) {
-                        logger.d(TAG, "Bluetooth Sco Audio disconnected")
+                ACTION_ACL_DISCONNECTED -> {
+                    intent.getHeadsetDevice()?.let { bluetoothDevice ->
+                        logger.d(
+                                TAG,
+                                "Bluetooth ACL device " +
+                                        bluetoothDevice.name +
+                                        " disconnected")
+                        deviceListener.onBluetoothDisconnected()
                     }
                 }
+                ACTION_SCO_AUDIO_STATE_UPDATED -> {
+                    intent.getIntExtra(EXTRA_SCO_AUDIO_STATE, SCO_AUDIO_STATE_ERROR).let { state ->
+                        when (state) {
+                            SCO_AUDIO_STATE_CONNECTED -> {
+                                logger.d(TAG, "Bluetooth SCO Audio connected")
+                            }
+                            SCO_AUDIO_STATE_DISCONNECTED -> {
+                                logger.d(TAG, "Bluetooth SCO Audio disconnected")
+                            }
+                            SCO_AUDIO_STATE_ERROR -> {
+                                logger.e(TAG, "Error retrieving Bluetooth SCO Audio state")
+                            }
+                        }
+                    }
+                }
+                else -> {}
             }
         }
     }
 
-    private fun isHeadsetDevice(bluetoothDevice: BluetoothDevice): Boolean {
-        val bluetoothClass = bluetoothDevice.bluetoothClass
-        if (bluetoothClass != null) {
-            val deviceClass = bluetoothClass.deviceClass
-            return deviceClass == BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE || deviceClass == BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET || deviceClass == BluetoothClass.Device.AUDIO_VIDEO_CAR_AUDIO
-        }
-        return false
-    }
+    private fun Intent.getHeadsetDevice(): BluetoothDevice? =
+        getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                ?.let { device -> if (isHeadsetDevice(device)) device else null }
+
+    private fun isHeadsetDevice(device: BluetoothDevice): Boolean =
+        device.bluetoothClass?.let { bluetoothClass ->
+            bluetoothClass.deviceClass.let { deviceClass ->
+                deviceClass == AUDIO_VIDEO_HANDSFREE ||
+                deviceClass == AUDIO_VIDEO_WEARABLE_HEADSET ||
+                deviceClass == AUDIO_VIDEO_CAR_AUDIO
+            }
+        } ?: false
 }
