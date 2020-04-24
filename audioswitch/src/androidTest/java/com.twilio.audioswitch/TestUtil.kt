f15b9a9c41aa3043d4d8b4dc3477e4218182a99f
John Qualls <jaltonqualls@twilio.com>
2020-04-24 15:15:13 -0600

    Fix broken test util after removing bluetooth controller static factory

--- audioswitch/src/androidTest/java/com.twilio.audioswitch/TestUtil.kt
+++ audioswitch/src/androidTest/java/com.twilio.audioswitch/TestUtil.kt
@@ -1,14 +1,17 @@
 package com.twilio.audioswitch
 
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.Context
 import android.content.Intent
 import android.media.AudioManager
+import com.twilio.audioswitch.android.BluetoothIntentProcessorImpl
 import com.twilio.audioswitch.android.BuildWrapper
 import com.twilio.audioswitch.android.FakeBluetoothIntentProcessor
 import com.twilio.audioswitch.android.LogWrapper
 import com.twilio.audioswitch.bluetooth.BluetoothController
 import com.twilio.audioswitch.bluetooth.BluetoothHeadsetReceiver
+import com.twilio.audioswitch.bluetooth.PreConnectedDeviceListener
 import com.twilio.audioswitch.selection.AudioDeviceManager
 import com.twilio.audioswitch.selection.AudioDeviceSelector
 import com.twilio.audioswitch.selection.AudioFocusRequestWrapper
@@ -28,12 +31,16 @@ internal fun setupFakeAudioDeviceSelector(context: Context):
     val wiredHeadsetReceiver = WiredHeadsetReceiver(context, logger)
     val bluetoothIntentProcessor = FakeBluetoothIntentProcessor()
     val bluetoothHeadsetReceiver = BluetoothHeadsetReceiver(context, logger, bluetoothIntentProcessor)
-    val bluetoothController = BluetoothController.newInstance(
-            context,
-            logger,
-            audioDeviceManager,
-            bluetoothIntentProcessor,
-            bluetoothHeadsetReceiver)
+    val bluetoothController = BluetoothAdapter.getDefaultAdapter()?.let { bluetoothAdapter ->
+        BluetoothController(context,
+                audioDeviceManager,
+                bluetoothAdapter,
+                PreConnectedDeviceListener(logger, bluetoothAdapter),
+                BluetoothHeadsetReceiver(context, logger, BluetoothIntentProcessorImpl())
+        )
+    } ?: run {
+        null
+    }
     return Pair(AudioDeviceSelector(logger,
             audioDeviceManager,
             wiredHeadsetReceiver,
