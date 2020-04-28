f52d6931fe23d179ab6daff967a5182fa2b96035
John Qualls <jaltonqualls@twilio.com>
2020-04-28 16:23:45 -0600

    Remove the switch gradle module

--- audioswitch/src/main/java/com/twilio/audioswitch/android/BluetoothIntentProcessor.kt
+++
@@ -1,8 +0,0 @@
-package com.twilio.audioswitch.android
-
-import android.content.Intent
-
-internal interface BluetoothIntentProcessor {
-
-    fun getBluetoothDevice(intent: Intent): BluetoothDeviceWrapper?
-}
--- audioswitch/src/main/java/com/twilio/audioswitch/android/BuildWrapper.kt
+++
@@ -1,8 +0,0 @@
-package com.twilio.audioswitch.android
-
-import android.os.Build
-
-internal class BuildWrapper {
-
-    fun getVersion(): Int = Build.VERSION.SDK_INT
-}
