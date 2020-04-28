f922ffe10254dc49b3ed6e8ef3ae8d803323a339
John Qualls <jaltonqualls@twilio.com>
2020-04-28 17:04:38 -0600

    Replace AudioSwitch module with Gradle dependency (#81)
    * Remove the switch gradle module
    
    * Add public audioswitch 0.1.0 dependency
    
    * Remove audioswitch CI jobs

--- audioswitch/src/main/java/com/twilio/audioswitch/android/BluetoothDeviceWrapper.kt
+++
@@ -1,7 +0,0 @@
-package com.twilio.audioswitch.android
-
-internal interface BluetoothDeviceWrapper {
-
-    val name: String
-    val deviceClass: Int?
-}
--- audioswitch/src/main/java/com/twilio/audioswitch/wired/WiredDeviceConnectionListener.kt
+++
@@ -1,6 +0,0 @@
-package com.twilio.audioswitch.wired
-
-internal interface WiredDeviceConnectionListener {
-    fun onDeviceConnected()
-    fun onDeviceDisconnected()
-}
