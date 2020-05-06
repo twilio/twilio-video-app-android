a13440354c62e0f33bc8d2cfa1a8e91243777b31
John Qualls <jaltonqualls@twilio.com>
2020-05-06 07:16:06 -0600

    Revert RoomEvent data class refactor and add ParticipantViewState as a property to NewRemoteVideoTrack

--- app/src/main/java/com/twilio/video/app/ui/room/RoomEvent.kt
+++ app/src/main/java/com/twilio/video/app/ui/room/RoomEvent.kt
@@ -3,17 +3,16 @@ package com.twilio.video.app.ui.room
 import com.twilio.video.RemoteParticipant
 import com.twilio.video.Room
 import com.twilio.video.app.data.api.AuthServiceError
+import com.twilio.video.app.participant.ParticipantViewState
 
-sealed class RoomEvent {
+sealed class RoomEvent(val room: Room? = null) {
 
-    abstract val room: Room?
-
-    object Connecting : RoomEvent() { override val room: Room? = null }
-    data class TokenError(override val room: Room? = null, val serviceError: AuthServiceError? = null) : RoomEvent()
-    data class RoomState(override val room: Room) : RoomEvent()
-    data class ConnectFailure(override val room: Room) : RoomEvent()
-    data class ParticipantConnected(override val room: Room, val remoteParticipant: RemoteParticipant) : RoomEvent()
-    data class NewRemoteVideoTrack(override val room: Room? = null, val remoteParticipant: RemoteParticipant) : RoomEvent()
-    data class ParticipantDisconnected(override val room: Room, val remoteParticipant: RemoteParticipant) : RoomEvent()
-    data class DominantSpeakerChanged(override val room: Room, val remoteParticipant: RemoteParticipant?) : RoomEvent()
+    object Connecting : RoomEvent()
+    class TokenError(val serviceError: AuthServiceError? = null) : RoomEvent()
+    class RoomState(room: Room) : RoomEvent(room)
+    class ConnectFailure(room: Room) : RoomEvent(room)
+    class ParticipantConnected(room: Room, val remoteParticipant: RemoteParticipant) : RoomEvent(room)
+    class NewRemoteVideoTrack(val participantViewState: ParticipantViewState) : RoomEvent()
+    class ParticipantDisconnected(room: Room, val remoteParticipant: RemoteParticipant) : RoomEvent(room)
+    class DominantSpeakerChanged(room: Room, val remoteParticipant: RemoteParticipant?) : RoomEvent(room)
 }
