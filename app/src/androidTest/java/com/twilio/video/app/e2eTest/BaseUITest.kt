abe712f6f1d46fcc456b6579aba1f813a23d847c
John Qualls <jaltonqualls@twilio.com>
2020-05-29 17:39:31 -0600

    Add setup for test orchestrator

--- app/src/androidTest/java/com/twilio/video/app/e2eTest/BaseUITest.kt
+++ app/src/androidTest/java/com/twilio/video/app/e2eTest/BaseUITest.kt
@@ -1,23 +1,20 @@
 package com.twilio.video.app.e2eTest
 
-import com.twilio.video.app.util.retrieveEmailCredentials
-import com.twilio.video.app.util.retryEspressoAction
-import com.twilio.video.app.screen.clickSettingsMenuItem
+import androidx.test.rule.GrantPermissionRule
 import com.twilio.video.app.screen.loginWithEmail
-import com.twilio.video.app.screen.logout
-import org.junit.After
+import com.twilio.video.app.util.retrieveEmailCredentials
 import org.junit.Before
+import org.junit.Rule
 
 open class BaseUITest {
+
+    @get:Rule
+    var permissionRule = GrantPermissionRule.grant(android.Manifest.permission.CAMERA,
+            android.Manifest.permission.RECORD_AUDIO,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
     @Before
     fun setUp() {
         loginWithEmail(retrieveEmailCredentials())
-    }
-
-    @After
-    fun tearDown() {
-        retryEspressoAction { clickSettingsMenuItem() }
-        logout()
     }
 }
