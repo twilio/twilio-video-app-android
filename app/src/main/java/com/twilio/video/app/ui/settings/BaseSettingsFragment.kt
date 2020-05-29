a24ca562558c2f4500dbeb87396eddba0aeb70f2
Aaron Alaniz <aalaniz@twilio.com>
2020-05-29 09:29:40 -0500

    Refactor setting default list preference values

--- app/src/main/java/com/twilio/video/app/ui/settings/BaseSettingsFragment.kt
+++ app/src/main/java/com/twilio/video/app/ui/settings/BaseSettingsFragment.kt
@@ -1,14 +1,22 @@
 package com.twilio.video.app.ui.settings
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.fragment.app.DialogFragment
+import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.twilio.video.app.data.get
 import com.twilio.video.app.data.NumberPreference
 import com.twilio.video.app.data.NumberPreferenceDialogFragmentCompat
 import dagger.android.support.AndroidSupportInjection
+import javax.inject.Inject
 
 abstract class BaseSettingsFragment : PreferenceFragmentCompat() {
+
+    @Inject
+    internal lateinit var sharedPreferences: SharedPreferences
+
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
@@ -31,5 +39,11 @@ abstract class BaseSettingsFragment : PreferenceFragmentCompat() {
         } else {
             super.onDisplayPreferenceDialog(preference)
         }
+    }
+
+    protected fun setListPreferenceValue(arrayId: Int, key: String, defaultValue: String) {
+        val defaultValueIndex = resources
+            .getStringArray(arrayId).indexOf(sharedPreferences.get(key, defaultValue))
+        (findPreference(key) as ListPreference).setValueIndex(defaultValueIndex)
     }
 }
