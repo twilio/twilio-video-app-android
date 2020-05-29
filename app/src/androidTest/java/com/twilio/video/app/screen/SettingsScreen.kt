06abd505682c283f03b8f2fd168e8d9510e43152
John Qualls <jaltonqualls@twilio.com>
2020-05-29 17:40:48 -0600

    Add bandwidth profile default ete test

--- app/src/androidTest/java/com/twilio/video/app/screen/SettingsScreen.kt
+++ app/src/androidTest/java/com/twilio/video/app/screen/SettingsScreen.kt
@@ -1,15 +1,24 @@
 package com.twilio.video.app.screen
 
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItem
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollTo
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.matcher.ViewMatchers.hasSibling
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.twilio.video.app.R
+import com.twilio.video.app.data.Preferences.BANDWIDTH_PROFILE_MAX_SUBSCRIPTION_BITRATE_DEFAULT
+import com.twilio.video.app.data.Preferences.BANDWIDTH_PROFILE_MAX_VIDEO_TRACKS_DEFAULT
 import com.twilio.video.app.util.getString
+import com.twilio.video.app.util.getStringArray
 import com.twilio.video.app.util.retryEspressoAction
 
 fun assertSettingsTitleIsVisible() {
@@ -17,8 +26,8 @@ fun assertSettingsTitleIsVisible() {
 }
 
 fun logout() {
-    onView(ViewMatchers.withId(androidx.preference.R.id.recycler_view))
-            .perform(RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
-                    ViewMatchers.hasDescendant(withText(
-                            getString(R.string.settings_screen_logout))), ViewActions.click()))
+    onView(withId(androidx.preference.R.id.recycler_view))
+            .perform(actionOnItem<ViewHolder>(
+                    hasDescendant(withText(
+                            getString(R.string.settings_screen_logout))), click()))
 }
