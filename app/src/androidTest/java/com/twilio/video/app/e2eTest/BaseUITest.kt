7bdc3931d5727d70f2e6fb1a96b96875aedb7db6
John Qualls <jaltonqualls@twilio.com>
2020-06-01 15:27:22 -0600

    Remove test orchestrator to resolve FTL errors

--- app/src/androidTest/java/com/twilio/video/app/e2eTest/BaseUITest.kt
+++ app/src/androidTest/java/com/twilio/video/app/e2eTest/BaseUITest.kt
@@ -1,8 +1,12 @@
 package com.twilio.video.app.e2eTest
 
 import androidx.test.rule.GrantPermissionRule
+import com.twilio.video.app.screen.clickSettingsMenuItem
 import com.twilio.video.app.screen.loginWithEmail
+import com.twilio.video.app.screen.logout
 import com.twilio.video.app.util.retrieveEmailCredentials
+import com.twilio.video.app.util.retryEspressoAction
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 
@@ -16,5 +20,11 @@ open class BaseUITest {
     @Before
     fun setUp() {
         loginWithEmail(retrieveEmailCredentials())
+    }
+
+    @After
+    fun tearDown() {
+        retryEspressoAction { clickSettingsMenuItem() }
+        logout()
     }
 }
