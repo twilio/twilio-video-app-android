c3743ed8740a1613382d48257e64d7cda1826eee
John Qualls <jaltonqualls@twilio.com>
2020-06-02 16:44:18 -0600

    Add permission e2e test class

---
+++ app/src/androidTest/java/com/twilio/video/app/e2eTest/PermissionTest.kt
@@ -0,0 +1,35 @@
+package com.twilio.video.app.e2eTest
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.activityScenarioRule
+import com.twilio.video.app.HiddenView
+import com.twilio.video.app.R
+import com.twilio.video.app.screen.loginWithEmail
+import com.twilio.video.app.ui.splash.SplashActivity
+import com.twilio.video.app.util.allowPermission
+import com.twilio.video.app.util.retrieveEmailCredentials
+import com.twilio.video.app.util.retryEspressoAction
+import com.twilio.video.app.util.uiDevice
+import org.junit.Rule
+import org.junit.Test
+
+class PermissionTest {
+
+    @get:Rule
+    var scenario = activityScenarioRule<SplashActivity>()
+
+    @Test
+    fun `it_should_render_the_local_video_track_before_connecting_to_a_room`() {
+        loginWithEmail(retrieveEmailCredentials())
+
+        uiDevice().run {
+            retryEspressoAction { allowPermission() }
+            retryEspressoAction { allowPermission() }
+            retryEspressoAction { allowPermission() }
+        }
+
+        retryEspressoAction { onView(withId(R.id.participant_stub_image))
+                .check(HiddenView()) }
+    }
+}
--- app/src/androidTest/java/com/twilio/video/app/util/UiAutomatorExtensions.kt
+++ app/src/androidTest/java/com/twilio/video/app/util/UiAutomatorExtensions.kt
@@ -1,6 +1,21 @@
 package com.twilio.video.app.util
 
+import android.os.Build
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
 
 fun uiDevice(): UiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
+fun UiDevice.allowPermission() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val device = uiDevice()
+        val allowPermissions = device.findObject(UiSelector()
+                .clickable(true)
+                .checkable(false)
+                .index(0))
+        if (allowPermissions.exists()) {
+            allowPermissions.click()
+        }
+    }
+}
