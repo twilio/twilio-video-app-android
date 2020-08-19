7e9ec1f31de6f05f8440da76e2ac543166bab65d
John Qualls <jaltonqualls@twilio.com>
2020-08-19 12:15:48 -0600

    Remove unnecessary module for BaseSettingsFragment

--- app/src/main/java/com/twilio/video/app/ui/settings/BaseSettingsFragment.kt
+++ app/src/main/java/com/twilio/video/app/ui/settings/BaseSettingsFragment.kt
@@ -1,6 +1,5 @@
 package com.twilio.video.app.ui.settings
 
-import android.content.Context
 import android.content.SharedPreferences
 import android.view.MenuItem
 import androidx.preference.ListPreference
@@ -10,18 +9,10 @@ import com.twilio.video.app.base.BaseActivity
 import com.twilio.video.app.data.NumberPreference
 import com.twilio.video.app.data.NumberPreferenceDialogFragmentCompat
 import com.twilio.video.app.util.get
-import dagger.android.support.AndroidSupportInjection
-import javax.inject.Inject
 
 abstract class BaseSettingsFragment : PreferenceFragmentCompat() {
 
-    @Inject
-    internal lateinit var sharedPreferences: SharedPreferences
-
-    override fun onAttach(context: Context) {
-        AndroidSupportInjection.inject(this)
-        super.onAttach(context)
-    }
+    protected val sharedPreferences: SharedPreferences get() = preferenceManager.sharedPreferences
 
     override fun onResume() {
         super.onResume()
