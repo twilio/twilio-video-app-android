36dbafaa297789bc3b280fc60bf8bd4f8c8936f1
John Qualls <jaltonqualls@twilio.com>
2020-08-21 17:26:33 -0600

    Add uniflow compatibility into RoomViewModelTests

--- app/src/test/java/com/twilio/video/app/ui/room/RoomViewModelTest.kt
+++ app/src/test/java/com/twilio/video/app/ui/room/RoomViewModelTest.kt
@@ -1,7 +1,9 @@
 package com.twilio.video.app.ui.room
 
+import android.Manifest
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
 import com.twilio.video.RemoteVideoTrack
 import com.twilio.video.app.participant.ParticipantManager
 import com.twilio.video.app.participant.ParticipantViewState
@@ -9,13 +11,14 @@ import com.twilio.video.app.sdk.RoomManager
 import com.twilio.video.app.sdk.VideoClient
 import com.twilio.video.app.sdk.VideoTrackViewState
 import com.twilio.video.app.ui.room.RoomEvent.ParticipantEvent.TrackSwitchOff
-import com.twilio.video.app.util.MainCoroutineScopeRule
 import com.twilio.video.app.util.PermissionUtil
 import io.reactivex.schedulers.TestScheduler
 import io.uniflow.android.test.TestViewObserver
 import io.uniflow.android.test.createTestObserver
 import io.uniflow.test.rule.TestDispatchersRule
+import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.nullValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Rule
@@ -69,93 +72,93 @@ class RoomViewModelTest {
         assertThat(updatedParticipant, equalTo(expectedParticipantViewState))
     }
 
-//    @Test
-//    fun `The TrackSwitchOff event should create a new VideoTrackViewState for an existing ParticipantViewState with the switch off set to true`() {
-//        val expectedVideoTrack = mock<RemoteVideoTrack>()
-//
-//        roomManager.sendParticipantEvent(TrackSwitchOff(PARTICIPANT_SID, expectedVideoTrack, true))
-//        scheduler.triggerActions()
-//
-//        val expectedTrackViewState = VideoTrackViewState(expectedVideoTrack, true)
-//        val expectedParticipantViewState = participantViewState.copy(
-//                videoTrack = expectedTrackViewState)
-//        val updatedParticipant = viewModel.viewState.value?.participantThumbnails?.find {
-//            it.sid == PARTICIPANT_SID
-//        }
-//        assertThat(updatedParticipant, equalTo(expectedParticipantViewState))
-//    }
-//
-//    @Test
-//    fun `The CheckLocalMedia event should set the isCameraEnabled view state property to true if camera permission is allowed`() {
-//        whenever(permissionUtil.isPermissionGranted(Manifest.permission.CAMERA))
-//                .thenReturn(true)
-//        val expectedViewState = RoomViewState(isCameraEnabled = true)
-//
-//        viewModel.processInput(RoomViewEvent.CheckPermissions)
-//
-//        assertThat(viewModel.viewState.value, equalTo(expectedViewState))
-//    }
-//
-//    @Test
-//    fun `The CheckLocalMedia event should set the isCameraEnabled view state property to false if camera permission is denied`() {
-//        viewModel = RoomViewModel(
-//                roomManager,
-//                mock(),
-//                permissionUtil,
-//                participantManager,
-//                scheduler = scheduler,
-//                initialViewState = RoomViewState(isCameraEnabled = true))
-//        whenever(permissionUtil.isPermissionGranted(Manifest.permission.CAMERA))
-//                .thenReturn(false)
-//        val expectedViewState = RoomViewState(isCameraEnabled = false)
-//
-//        viewModel.processInput(RoomViewEvent.CheckPermissions)
-//
-//        assertThat(viewModel.viewState.value, equalTo(expectedViewState))
-//        assertThat(viewModel.viewEffects.value, `is`(nullValue()))
-//    }
-//
-//    @Test
-//    fun `The CheckLocalMedia event should set the isMicEnabled view state property to true if camera permission is allowed`() {
-//        whenever(permissionUtil.isPermissionGranted(Manifest.permission.RECORD_AUDIO))
-//                .thenReturn(true)
-//        val expectedViewState = RoomViewState(isMicEnabled = true)
-//
-//        viewModel.processInput(RoomViewEvent.CheckPermissions)
-//
-//        assertThat(viewModel.viewState.value, equalTo(expectedViewState))
-//    }
-//
-//    @Test
-//    fun `The CheckLocalMedia event should set the isMicEnabled view state property to false if camera permission is denied`() {
-//        viewModel = RoomViewModel(
-//                roomManager,
-//                mock(),
-//                permissionUtil,
-//                participantManager,
-//                scheduler = scheduler,
-//                initialViewState = RoomViewState(isMicEnabled = true))
-//        whenever(permissionUtil.isPermissionGranted(Manifest.permission.RECORD_AUDIO))
-//                .thenReturn(false)
-//        val expectedViewState = RoomViewState(isMicEnabled = false)
-//
-//        viewModel.processInput(RoomViewEvent.CheckPermissions)
-//
-//        assertThat(viewModel.viewState.value, equalTo(expectedViewState))
-//        assertThat(viewModel.viewEffects.value, `is`(nullValue()))
-//    }
-//
-//    @Test
-//    fun `The CheckLocalMedia event should send a CheckLocalMedia ViewEffect if camera and mic permissions are allowed`() {
-//        whenever(permissionUtil.isPermissionGranted(Manifest.permission.CAMERA))
-//                .thenReturn(true)
-//        whenever(permissionUtil.isPermissionGranted(Manifest.permission.RECORD_AUDIO))
-//                .thenReturn(true)
-//
-//        viewModel.processInput(RoomViewEvent.CheckPermissions)
-//
-//        val expectedViewEffect = viewModel.viewEffects.value!!
-//                .getContentIfNotHandled() is RoomViewEffect.CheckLocalMedia
-//        assertThat(expectedViewEffect, equalTo(true))
-//    }
+    @Test
+    fun `The TrackSwitchOff event should create a new VideoTrackViewState for an existing ParticipantViewState with the switch off set to true`() {
+        val expectedVideoTrack = mock<RemoteVideoTrack>()
+
+        roomManager.sendParticipantEvent(TrackSwitchOff(PARTICIPANT_SID, expectedVideoTrack, true))
+        scheduler.triggerActions()
+
+        val expectedTrackViewState = VideoTrackViewState(expectedVideoTrack, true)
+        val expectedParticipantViewState = participantViewState.copy(
+                videoTrack = expectedTrackViewState)
+        val updatedParticipant = (viewModel.getCurrentState() as RoomViewState).participantThumbnails?.find {
+            it.sid == PARTICIPANT_SID
+        }
+        assertThat(updatedParticipant, equalTo(expectedParticipantViewState))
+    }
+
+    @Test
+    fun `The CheckLocalMedia event should set the isCameraEnabled view state property to true if camera permission is allowed`() {
+        whenever(permissionUtil.isPermissionGranted(Manifest.permission.CAMERA))
+                .thenReturn(true)
+        val expectedViewState = RoomViewState(isCameraEnabled = true)
+
+        viewModel.processInput(RoomViewEvent.CheckPermissions)
+
+        testObserver.verifySequence(RoomViewState(), expectedViewState)
+    }
+
+    @Test
+    fun `The CheckLocalMedia event should set the isCameraEnabled view state property to false if camera permission is denied`() {
+        viewModel = RoomViewModel(
+                roomManager,
+                mock(),
+                permissionUtil,
+                participantManager,
+                scheduler = scheduler,
+                initialViewState = RoomViewState(isCameraEnabled = true))
+        whenever(permissionUtil.isPermissionGranted(Manifest.permission.CAMERA))
+                .thenReturn(false)
+        val expectedViewState = RoomViewState(isCameraEnabled = false)
+
+        viewModel.processInput(RoomViewEvent.CheckPermissions)
+
+        testObserver.verifySequence(expectedViewState)
+
+        assertThat(testObserver.lastEventOrNull, `is`(nullValue()))
+    }
+
+    @Test
+    fun `The CheckLocalMedia event should set the isMicEnabled view state property to true if camera permission is allowed`() {
+        whenever(permissionUtil.isPermissionGranted(Manifest.permission.RECORD_AUDIO))
+                .thenReturn(true)
+        val expectedViewState = RoomViewState(isMicEnabled = true)
+
+        viewModel.processInput(RoomViewEvent.CheckPermissions)
+
+        testObserver.verifySequence(RoomViewState(), expectedViewState)
+    }
+
+    @Test
+    fun `The CheckLocalMedia event should set the isMicEnabled view state property to false if camera permission is denied`() {
+        viewModel = RoomViewModel(
+                roomManager,
+                mock(),
+                permissionUtil,
+                participantManager,
+                scheduler = scheduler,
+                initialViewState = RoomViewState(isMicEnabled = true))
+        whenever(permissionUtil.isPermissionGranted(Manifest.permission.RECORD_AUDIO))
+                .thenReturn(false)
+        val expectedViewState = RoomViewState(isMicEnabled = false)
+
+        viewModel.processInput(RoomViewEvent.CheckPermissions)
+
+        testObserver.verifySequence(expectedViewState)
+
+        assertThat(testObserver.lastEventOrNull, `is`(nullValue()))
+    }
+
+    @Test
+    fun `The CheckLocalMedia event should send a CheckLocalMedia ViewEffect if camera and mic permissions are allowed`() {
+        whenever(permissionUtil.isPermissionGranted(Manifest.permission.CAMERA))
+                .thenReturn(true)
+        whenever(permissionUtil.isPermissionGranted(Manifest.permission.RECORD_AUDIO))
+                .thenReturn(true)
+
+        viewModel.processInput(RoomViewEvent.CheckPermissions)
+
+        assertThat(testObserver.lastEventOrNull is RoomViewEffect.CheckLocalMedia, equalTo(true))
+    }
 }
