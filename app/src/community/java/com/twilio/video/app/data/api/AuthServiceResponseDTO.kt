7e1246139962d69f90bf0c1c1008abb82b4fdba0
twilio-sdk-build <twilio-sdk-build@twilio.com>
2020-08-24 18:32:09 +0000

    Revert "Add room type response type" [skip ci]
    This reverts commit 86aea2d72f825147e04abd8cefcee7c048be0936.
    
    Revert "Bump version code [skip ci]"
    
    This reverts commit d003cb96d86eb5d8068d4c25b7f2c29c650a3af6.

--- app/src/community/java/com/twilio/video/app/data/api/AuthServiceResponseDTO.kt
+++ app/src/community/java/com/twilio/video/app/data/api/AuthServiceResponseDTO.kt
@@ -1,8 +1,5 @@
 package com.twilio.video.app.data.api
 
-import com.google.gson.annotations.SerializedName
-
 data class AuthServiceResponseDTO(
-    val token: String? = null,
-    @SerializedName("room_type") val roomType: RoomType? = null
+    val token: String? = null
 )
--- app/src/community/java/com/twilio/video/app/data/api/RoomType.kt
+++
@@ -1,9 +0,0 @@
-package com.twilio.video.app.data.api
-
-import com.google.gson.annotations.SerializedName
-
-enum class RoomType {
-    @SerializedName("group") GROUP,
-    @SerializedName("group-small") GROUP_SMALL,
-    @SerializedName("peer-to-peer") PEER_TO_PEER
-}
