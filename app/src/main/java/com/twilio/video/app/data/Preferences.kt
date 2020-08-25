2d5c45538ffffd5212f495fb01365003776df500
John Qualls <jaltonqualls@twilio.com>
2020-08-25 13:51:28 -0600

    AHOYAPPS-829: Optimize for P2P (#132)
    * Add room type response type
    
    * Remove unused version code constant
    
    * Implement logic to handle room type from server response
    
    * Replace preferences editor extension functions with android ktx edit function
    
    * Fix lint error

--- app/src/main/java/com/twilio/video/app/data/Preferences.kt
+++ app/src/main/java/com/twilio/video/app/data/Preferences.kt
@@ -30,14 +30,13 @@ object Preferences {
     const val ENVIRONMENT = "pref_environment"
     const val ENVIRONMENT_DEFAULT = BuildConfig.ENVIRONMENT_DEFAULT
     const val TOPOLOGY = "pref_topology"
-    val TOPOLOGY_DEFAULT: String = Topology.GROUP.string
+    val TOPOLOGY_DEFAULT: String = Topology.GROUP.value
     const val MIN_FPS = "pref_min_fps"
     const val MAX_FPS = "pref_max_fps"
     const val MIN_VIDEO_DIMENSIONS = "pref_min_video_dim"
     const val MAX_VIDEO_DIMENSIONS = "pref_max_video_dim"
     const val ASPECT_RATIO = "pref_aspect_ratio"
     const val VERSION_NAME = "pref_version_name"
-    const val VERSION_CODE = "pref_version_code"
     const val VIDEO_LIBRARY_VERSION = "pref_video_library_version"
     const val LOGOUT = "pref_logout"
     const val ENABLE_STATS = "pref_enable_stats"
--- app/src/main/java/com/twilio/video/app/data/api/model/Topology.kt
+++ app/src/main/java/com/twilio/video/app/data/api/model/Topology.kt
@@ -15,6 +15,16 @@
  */
 package com.twilio.video.app.data.api.model
 
-enum class Topology(val string: String) {
-    GROUP("group"), GROUP_SMALL("group-small"), P2P("peer-to-peer");
+import com.google.gson.annotations.SerializedName
+
+private const val GROUP_ROOM_NAME = "group"
+private const val GROUP_SMALL_ROOM_NAME = "group-small"
+private const val PEER_TO_PEER_ROOM_NAME = "peer-to-peer"
+
+enum class Topology(val value: String) {
+    @SerializedName(GROUP_ROOM_NAME) GROUP(GROUP_ROOM_NAME),
+    @SerializedName(GROUP_SMALL_ROOM_NAME) GROUP_SMALL(GROUP_SMALL_ROOM_NAME),
+    @SerializedName(PEER_TO_PEER_ROOM_NAME) PEER_TO_PEER(PEER_TO_PEER_ROOM_NAME);
+
+    override fun toString() = value
 }
