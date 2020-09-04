be2e89d93896838c247875895a9b6aecb50de0b9
John Qualls <jaltonqualls@twilio.com>
2020-09-04 14:29:51 -0600

    Add unit tests

--- app/src/community/java/com/twilio/video/app/data/api/AuthServiceRepository.kt
+++ app/src/community/java/com/twilio/video/app/data/api/AuthServiceRepository.kt
@@ -17,7 +17,7 @@ package com.twilio.video.app.data.api
 
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
-import com.twilio.video.VideoDimensions
+import com.twilio.video.VideoDimensions.HD_720P_VIDEO_DIMENSIONS
 import com.twilio.video.Vp8Codec
 import com.twilio.video.app.android.SharedPreferencesWrapper
 import com.twilio.video.app.data.PASSCODE
@@ -93,7 +93,7 @@ class AuthServiceRepository(
                     sharedPreferences.edit { putString(TOPOLOGY, serverTopology.value) }
                     val (enableSimulcast, minVideoDimensions) = when (serverTopology) {
                         GROUP, GROUP_SMALL -> true to 0
-                        PEER_TO_PEER -> true to VIDEO_DIMENSIONS.indexOf(VideoDimensions.HD_720P_VIDEO_DIMENSIONS)
+                        PEER_TO_PEER -> false to VIDEO_DIMENSIONS.indexOf(HD_720P_VIDEO_DIMENSIONS)
                     }
                     Timber.d("Server topology has changed to %s. Setting the codec to Vp8 with simulcast set to %s",
                             serverTopology, enableSimulcast)
