5ff1745eba1a711a9a17a01b4a4fe95348cf4347
John Qualls <jaltonqualls@twilio.com>
2020-10-12 13:18:26 -0600

    Finish topology selection integration test

--- app/src/androidTest/java/com/twilio/video/app/integrationTest/PreferenceIntegrationTest.kt
+++ app/src/androidTest/java/com/twilio/video/app/integrationTest/PreferenceIntegrationTest.kt
@@ -4,6 +4,7 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.scrollTo
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.hasSibling
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -13,11 +14,16 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.twilio.video.app.R
 import com.twilio.video.app.data.Preferences
+import com.twilio.video.app.data.api.model.Topology
 import com.twilio.video.app.ui.settings.SettingsActivity
 import com.twilio.video.app.util.assertTextIsDisplayedRetry
+import com.twilio.video.app.util.clickView
+import com.twilio.video.app.util.getSharedPreferences
 import com.twilio.video.app.util.getString
 import com.twilio.video.app.util.getStringArray
+import com.twilio.video.app.util.getTargetContext
 import com.twilio.video.app.util.scrollAndClickView
+import org.hamcrest.CoreMatchers.equalTo
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -49,6 +55,27 @@ class PreferenceIntegrationTest {
         assertTextIsDisplayedRetry(getString(R.string.settings_title_bandwidth_profile))
 
         assertDefaultBandwidthProfileSettings()
+    }
+
+    @Test
+    fun it_should_select_the_correct_topology() {
+        scrollAndClickView(getString(R.string.settings_title_advanced), R.id.recycler_view)
+
+        assertTextIsDisplayedRetry(getString(R.string.settings_title_advanced))
+
+        scrollAndClickView(getString(R.string.settings_title_internal), R.id.recycler_view)
+
+        val sharedPreferences = getSharedPreferences(getTargetContext())
+        var preference = sharedPreferences.getString(Preferences.TOPOLOGY, null)
+        assertThat(preference, equalTo(Topology.GROUP.value))
+
+        Topology.values().forEach { topology ->
+            scrollAndClickView(getString(R.string.settings_screen_topology), R.id.recycler_view)
+            clickView(topology.value)
+
+            preference = sharedPreferences.getString(Preferences.TOPOLOGY, null)
+            assertThat(preference, equalTo(topology.value))
+        }
     }
 
     private fun assertDefaultBandwidthProfileSettings() {
