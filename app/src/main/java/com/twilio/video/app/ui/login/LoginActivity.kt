45369b6c5dd393345b60e421b46f4293f58cb18c
John Qualls <jaltonqualls@twilio.com>
2020-12-04 17:08:09 -0700

    Add error handling for firebase UI

--- app/src/main/java/com/twilio/video/app/ui/login/LoginActivity.kt
+++ app/src/main/java/com/twilio/video/app/ui/login/LoginActivity.kt
@@ -20,12 +20,13 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseAuth
 import com.twilio.video.app.R
 import com.twilio.video.app.base.BaseActivity
+import com.twilio.video.app.ui.room.RoomActivity
 import timber.log.Timber
 
 private const val RC_SIGN_IN = 20
@@ -44,19 +45,13 @@ class LoginActivity : BaseActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == RC_SIGN_IN) {
-            val response = IdpResponse.fromResultIntent(data)
-
             if (resultCode == Activity.RESULT_OK) {
-                // Successfully signed in
-                val user = FirebaseAuth.getInstance().currentUser
-                Timber.d("Log in was successful!!")
-                // ...
+                startLobbyActivity()
             } else {
-                Timber.d("Log in was not successful :(")
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
+                IdpResponse.fromResultIntent(data)?.let {
+                    Timber.e(it.error)
+                    showAuthErrorDialog()
+                }
             }
         }
     }
@@ -78,5 +73,18 @@ class LoginActivity : BaseActivity() {
                         .setAlwaysShowSignInMethodScreen(true)
                         .build(),
                 RC_SIGN_IN)
+    }
+
+    private fun showAuthErrorDialog() {
+        AlertDialog.Builder(this, R.style.AppTheme_Dialog)
+                .setTitle(getString(R.string.login_screen_auth_error_title))
+                .setMessage(getString(R.string.login_screen_auth_error_desc))
+                .setPositiveButton("OK", null)
+                .show()
+    }
+
+    private fun startLobbyActivity() {
+        RoomActivity.startActivity(this, intent.data)
+        finish()
     }
 }
