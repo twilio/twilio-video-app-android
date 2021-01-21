c43b096b0a87aed6aa0fef0452ba596a663e75f4
John Qualls <jaltonqualls@twilio.com>
2021-01-20 17:23:37 -0700

    Remove rx java composable from the video service

--- app/src/main/java/com/twilio/video/app/ui/room/VideoService.kt
+++ app/src/main/java/com/twilio/video/app/ui/room/VideoService.kt
@@ -6,17 +6,13 @@ import android.content.Intent
 import android.os.Build
 import android.os.IBinder
 import com.twilio.video.app.sdk.RoomManager
-import com.twilio.video.app.util.plus
 import dagger.android.AndroidInjection
-import io.reactivex.disposables.CompositeDisposable
-import javax.inject.Inject
 import timber.log.Timber
+import javax.inject.Inject
 
 private const val ROOM_NAME_EXTRA = "ROOM_NAME_EXTRA"
 
-class VideoService(
-    private val rxDisposables: CompositeDisposable = CompositeDisposable()
-) : Service() {
+class VideoService : Service() {
 
     companion object {
         fun startService(context: Context, roomName: String) {
@@ -52,7 +48,6 @@ class VideoService(
     override fun onDestroy() {
         super.onDestroy()
         Timber.d("VideoService destroyed")
-        rxDisposables.clear()
     }
 
     override fun onBind(intent: Intent?): IBinder? {
