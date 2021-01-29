bbb2ec983c731344414074024c311233528cc932
John Qualls <jaltonqualls@twilio.com>
2021-01-29 11:54:25 -0700

    Fix tests after adding firebase ui

--- app/src/main/java/com/twilio/video/app/ui/login/LoginActivity.kt
+++ app/src/main/java/com/twilio/video/app/ui/login/LoginActivity.kt
@@ -20,11 +20,8 @@ import android.app.Activity
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.app.AlertDialog
 import androidx.core.content.edit
 import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -34,45 +31,33 @@ import com.twilio.video.app.data.Preferences
 import com.twilio.video.app.databinding.LoginActivityBinding
 import com.twilio.video.app.ui.room.RoomActivity
 import javax.inject.Inject
-import timber.log.Timber
 
 private const val RC_SIGN_IN = 20
 
 class LoginActivity : BaseActivity() {
 
     private lateinit var authUI: FirebaseAuth
-    private lateinit var binding: LoginActivityBinding
-
     @Inject
     internal lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = LoginActivityBinding.inflate(layoutInflater)
+        setContentView(LoginActivityBinding.inflate(layoutInflater).root)
         authUI = FirebaseAuth.getInstance()
-        setContentView(binding.root)
-        binding.signInButton.setOnClickListener {
-            binding.progressBar.visibility = View.VISIBLE
-            navigateToFirebaseUIAuth()
-        }
+        navigateToFirebaseUIAuth()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == RC_SIGN_IN) {
-            binding.progressBar.visibility = View.GONE
             if (resultCode == Activity.RESULT_OK) {
                 authUI.currentUser?.let { user ->
                     saveIdentity(user)
                     startLobbyActivity()
                 }
-            } else {
-                IdpResponse.fromResultIntent(data)?.let {
-                    Timber.e(it.error)
-                    showAuthErrorDialog()
-                }
             }
+            finish()
         }
     }
 
@@ -94,14 +79,6 @@ class LoginActivity : BaseActivity() {
                         .setTheme(R.style.GreenTheme)
                         .build(),
                 RC_SIGN_IN)
-    }
-
-    private fun showAuthErrorDialog() {
-        AlertDialog.Builder(this, R.style.AppTheme_Dialog)
-                .setTitle(getString(R.string.login_screen_auth_error_title))
-                .setMessage(getString(R.string.login_screen_auth_error_desc))
-                .setPositiveButton("OK", null)
-                .show()
     }
 
     private fun startLobbyActivity() {
