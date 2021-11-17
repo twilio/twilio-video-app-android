fbb4802e3cf10c355b21d42a1656a30201fc0485
Magnus Martikainen <mmartikainen@twilio.com>
2021-11-17 15:57:21 -0800

    Try running tests even if permissions dialog was not shown

--- app/src/androidTest/java/com/twilio/video/app/e2eTest/BaseE2ETest.kt
+++ app/src/androidTest/java/com/twilio/video/app/e2eTest/BaseE2ETest.kt
@@ -1,9 +1,11 @@
 package com.twilio.video.app.e2eTest
 
+import android.util.Log
 import com.twilio.video.app.screen.loginWithEmail
 import com.twilio.video.app.util.allowAllPermissions
 import com.twilio.video.app.util.retrieveEmailCredentials
 import com.twilio.video.app.util.uiDevice
+import java.util.concurrent.TimeoutException
 import org.junit.Before
 
 @E2ETest
@@ -12,7 +14,12 @@ open class BaseE2ETest {
     open fun setUp() {
         loginWithEmail(retrieveEmailCredentials())
         uiDevice().run {
-            allowAllPermissions()
+            try {
+                allowAllPermissions()
+            } catch (e: TimeoutException) {
+                Log.w("VideoApiUtils", "Permissions dialog not detected")
+                // try running the test anyway
+            }
         }
     }
 }
