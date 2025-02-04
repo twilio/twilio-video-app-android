b5a39996c979f6ebd7daac198916866308cd2d3b
afalls-twilio <91512928+afalls-twilio@users.noreply.github.com>
2025-02-04 15:51:02 -0800

    Squashed 'videoapp/' changes from 41476c7bc..e1ba9b446 (#839)
    updated ahoy subtree

--- app/src/main/java/com/twilio/video/app/data/api/dto/AuthServiceRequestDTO.kt
+++ app/src/main/java/com/twilio/video/app/data/api/dto/AuthServiceRequestDTO.kt
@@ -1,8 +1,10 @@
 package com.twilio.video.app.data.api
 
+import com.google.gson.annotations.SerializedName
+
 data class AuthServiceRequestDTO(
-    val passcode: String? = null,
-    val user_identity: String? = null,
-    val room_name: String? = null,
-    val create_room: Boolean = false,
+    @SerializedName("passcode") val passcode: String? = null,
+    @SerializedName("user_identity") val user_identity: String? = null,
+    @SerializedName("room_name") val room_name: String? = null,
+    @SerializedName("create_room") val create_room: Boolean = false,
 )
