b0de2a4dd39c200cb5b76588aaf3697168f8e331
Ardavon Falls <afalls@twilio.com>
2025-09-11 16:05:17 -0700

    Updated to use androidx.crypto instead of facebook conseal

--- app/src/community/java/com/twilio/video/app/security/SecurePreferencesImpl.kt
+++ app/src/community/java/com/twilio/video/app/security/SecurePreferencesImpl.kt
@@ -3,42 +3,37 @@ package com.twilio.video.app.security
 import android.content.Context
 import android.content.SharedPreferences
 import android.util.Base64
-import com.facebook.android.crypto.keychain.AndroidConceal
-import com.facebook.android.crypto.keychain.SharedPrefsBackedKeyChain
-import com.facebook.crypto.Crypto
-import com.facebook.crypto.CryptoConfig
-import com.facebook.crypto.Entity
-import com.facebook.soloader.SoLoader
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import androidx.core.content.edit
 
 class SecurePreferencesImpl(
     context: Context,
-    private val preferences: SharedPreferences,
+    preferences: SharedPreferences,
 ) : SecurePreferences {
 
-    private val entity: Entity = Entity.create(context.packageName)
-    private val crypto: Crypto
+    private val securePreferences: SharedPreferences
 
     init {
-        SoLoader.init(context, false)
-        val keyChain = SharedPrefsBackedKeyChain(context, CryptoConfig.KEY_256)
-        crypto = AndroidConceal.get().createCrypto256Bits(keyChain)
+        val masterKey = MasterKey.Builder(context)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        securePreferences = EncryptedSharedPreferences.create(
+            context,
+            "secure_prefs",
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
     }
 
     override fun putSecureString(key: String, value: String) {
-        return preferences.edit().putString(key, encrypt(value)).apply()
+        securePreferences.edit { putString(key, value) }
     }
 
     override fun getSecureString(key: String): String? {
-        val encryptedText: String? = preferences.getString(key, null)
-        return if (encryptedText != null) decrypt(encryptedText) else null
+        return securePreferences.getString(key, null)
     }
 
-    private fun encrypt(plainText: String): String {
-        val cipherText = crypto.encrypt(plainText.toByteArray(), entity)
-        return Base64.encodeToString(cipherText, Base64.DEFAULT)
-    }
-
-    private fun decrypt(encryptedText: String): String {
-        return String(crypto.decrypt(Base64.decode(encryptedText, Base64.DEFAULT), entity))
-    }
 }
