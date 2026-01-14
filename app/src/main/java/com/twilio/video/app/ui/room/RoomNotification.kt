b3d75b841b3fb15da1799f5d0d5bd998ed091425
Ardavon Falls <afalls@twilio.com>
2026-01-14 13:37:52 -0800

    made spotless happy

--- app/src/main/java/com/twilio/video/app/ui/room/RoomNotification.kt
+++ app/src/main/java/com/twilio/video/app/ui/room/RoomNotification.kt
@@ -6,7 +6,6 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.twilio.video.app.R
 
